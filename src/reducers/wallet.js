7a37b9f322202373aa523f7cf56dd1b51130bda8
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-08 14:38:22 -0300

    feat: reducer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,30 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {
+  REQUEST_CURRENCIES,
+  WALLET_CURRENCIES,
+  FAILED_CURRENCIES,
+} from '../actions';
+
+const INITIAL_STATE = {
+  loading: false,
+  error: '',
+  currencies: [],
+  expenses: [],
+};
+
+function userWalletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case REQUEST_CURRENCIES:
+    return { ...state, loading: true };
+
+  case WALLET_CURRENCIES:
+    return { ...state, loading: false, currencies: action.payload };
+
+  case FAILED_CURRENCIES:
+    return { ...state, loading: false, error: action.payload };
+
+  default:
+    return state;
+  }
+}
+
+export default userWalletReducer;
