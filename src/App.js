0975ca0b4bb0ab3eeca2b7f1287808ad1f566792
Patrick Morais <ppternunes@gmail.com>
2021-06-08 19:07:31 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
+      <Route component={ NotFound } />
+    </Switch>
+  );
 }
 
 export default App;
