cef9d78d28716219ef19b6f2d76e94df624f0862
Alexandre Oliveira <alexandremolive@gmail.com>
2021-06-08 17:33:08 -0300

    req 1 parcial

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Bem Vindo a Trybe Wallet</h1>
+        <form>
+          <div>
+            <input data-testid="email-input" type="email" />
+            <input data-testid="password-input" type="password" />
+          </div>
+          <div>
+            <button type="button">Entrar</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 export default Login;
