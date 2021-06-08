50ca1a4d239539e3dcdfb6f6dff3c00dc1a8ec94
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-06-08 17:09:25 -0300

    :rocket: Requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="container">
+        <section className="logo">
+          logo
+        </section>
+        <main>
+          <form>
+            <input type="text" data-testid="email-input" placeholder="Email" />
+            <input type="password" data-testid="password-input" placeholder="Senha" />
+            <button type="button">Entrar</button>
+          </form>
+        </main>
+      </div>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'TESTE':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
