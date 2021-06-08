3f8009def00dc2357c64bbccbbe35aea7557b622
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-06-08 17:23:48 -0300

    Pagina Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          type="email"
+          placeholder="Email"
+          data-testid="email-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          minLength="6"
+          data-testid="password-input"
+          required
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </Link>
+      </form>
+    );
   }
 }
 
