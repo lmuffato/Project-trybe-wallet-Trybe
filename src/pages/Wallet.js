af0b90e10880f1760873e39d630255ff9446b978
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-08 14:48:19 -0300

    feat: pagina da carteira 1 > 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-class Wallet extends React.Component {
-  render() {
-    return <div>TrybeWallet</div>;
-  }
-}
+import { fetchCurrency } from '../actions';
+
+const Wallet = () => {
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const dispatch = useDispatch();
+
+  const globalState = useSelector((state) => state);
+  const { user, wallet } = globalState;
+
+  useEffect(() => {
+    dispatch(fetchCurrency());
+  }, []);
+
+  return (
+    <section>
+      <header>
+        <div>
+          <p data-testid="email-field">{user.email}</p>
+          <p data-testid="total-field">{totalPrice}</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+
+        <div>
+          <input type="number" data-testid="value-input" />
+          <input data-testid="description-input" />
+          <select data-testid="currency-input">
+            {wallet.currencies.map((currency) => (
+              <option key={ currency } data-testid={ currency }>
+                {currency}
+              </option>
+            ))}
+          </select>
+        </div>
+      </header>
+    </section>
+  );
+};
 
 export default Wallet;
