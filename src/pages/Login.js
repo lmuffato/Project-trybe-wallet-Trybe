5fa9e3946d57f6d7d648ac72935f4a7813df9abd
guidugaich <guidugaich.dev@gmail.com>
2021-06-08 20:06:48 -0300

    criando componente Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="text"
+              name="email"
+              id="email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              name="email"
+              id="password"
+              data-testid="password-input"
+            />
+          </label>
+          <button type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
