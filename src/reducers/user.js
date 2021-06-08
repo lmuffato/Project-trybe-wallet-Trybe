9205a6230acf005d33608e35c752bf06071f6f02
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-08 14:00:47 -0400

    reducers criados

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SEND_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SEND_EMAIL:
+    return {
+      ...state,
+      email: action.payload,
+    };
+  default:
+    return state;
+  }
+}
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,41 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {
+  REQUEST_CURRENCIES, ADD_EXPENSE, IS_FETCHING, TOTAL_VALUE,
+} from '../actions';
+
+const INITIAL_STATE = {
+  isFetching: false,
+  currencies: [],
+  expenses: [],
+  id: -1,
+  totalValue: 0,
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case IS_FETCHING:
+    return {
+      ...state,
+      isFetching: true,
+    };
+  case REQUEST_CURRENCIES:
+    return {
+      ...state,
+      isFetching: false,
+      currencies: action.payload,
+    };
+  case ADD_EXPENSE:
+    return {
+      ...state,
+      isFetching: false,
+      expenses: [...state.expenses, action.payload.localState],
+      id: action.payload.id,
+    };
+  case TOTAL_VALUE:
+    return {
+      ...state,
+      totalValue: state.totalValue + action.payload,
+    };
+  default:
+    return state;
+  }
+}
