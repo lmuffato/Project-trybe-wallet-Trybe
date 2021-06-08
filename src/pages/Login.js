4c780969f36c7925c35711a959928cf788faae36
Samuel <samuelvanguarda@gmail.com>
2021-06-08 16:54:53 -0300

    Requisito 1 - Correçao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <forms>
+          <input data-testid="email-input" type="email" />
+          <input data-testid="password-input" type="password" />
+          <button type="submit">Entrar</button>
+        </forms>
+      </div>
+    );
   }
 }
 
