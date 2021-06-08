2128c6a2ec53c2f0c4696d3fc1e29594db0fee68
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-06-08 19:35:16 -0300

    store criado

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,17 +8,17 @@
 
 export const LOGIN = 'LOGIN';
 
-const login = () => ({
-  type: LOGIN,
-  payload,
-});
+// const login = () => ({
+//   type: LOGIN,
+//   payload,
+// });
 //
 
 export const ADD_OUTLAY = 'ADD_OUTLAY';
 export const REMOVE_OUTLAY = 'REMOVE_OUTLAY';
 export const EDIT_OUTLAY = 'EDIT_OUTLAY';
 
-const addOutlay = (payload) => ({
-  type: ADD_OUTLAY,
-  payload,
-});
+// const addOutlay = (payload) => ({
+//   type: ADD_OUTLAY,
+//   payload,
+// });
