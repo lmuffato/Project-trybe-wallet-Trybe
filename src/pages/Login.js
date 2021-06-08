b9d456c81eae635d1e772ba7b3f24cb2f73be02d
AndreBarroso <andre_barroso17@hotmail.com>
2021-06-08 19:08:42 -0300

    feat: requisito 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.habClick = this.habClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  habClick() {
+    let disable = true;
+    const smallerSize = 6;
+    const { email, password } = this.state;
+    const lengthPassword = password.length;
+    const rightEmail = /\w+@\w+.com$/.test(email);
+    if (rightEmail && lengthPassword >= smallerSize) disable = false;
+    else disable = true;
+    return disable;
+  }
+
   render() {
+    this.habClick();
+
     return (
       <form>
         <label htmlFor="email">
@@ -10,6 +41,8 @@ class Login extends React.Component {
             type="email"
             id="email"
             data-testid="email-input"
+            onChange={ this.handleChange }
+            name="email"
           />
         </label>
 
@@ -17,12 +50,14 @@ class Login extends React.Component {
           Senha:
           <input
             type="password"
-            data-testid="password-input"
             id="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            name="password"
           />
         </label>
 
-        <button type="button">Entrar</button>
+        <button type="button" disabled={ this.habClick() }>Entrar</button>
       </form>
     );
   }
