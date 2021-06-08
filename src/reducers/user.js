79c299f98b36fe62f5ee44e92849b1610e6c6bf0
mallmann02 <leomallmann020302@gmail.com>
2021-06-08 19:31:13 -0300

    feat: User reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initial_state = {};
+
+function userReducer(state = initial_state, action) {
+  switch(action.type) {
+    case 'LOGIN':
+      return action.payload
+    default:
+      return state
+  }
+}
+
+export default userReducer;
