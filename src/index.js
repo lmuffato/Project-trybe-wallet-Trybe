e572e3bf31aa2371d0b69d8dae645f8fdd155b22
Samuel <samuelvanguarda@gmail.com>
2021-06-08 16:46:56 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
 ReactDOM.render(
   <BrowserRouter>
