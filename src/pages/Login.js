95372480d3857f94cd01dd5ea596f2800e7b47ca
lucas-paz <lucas.hpaz@outlook.com>
2021-06-08 15:17:46 -0300

    cria formulario da tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Email
+            <input type="email" name="email" data-testid="email-input" />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input type="password" name="password" data-testid="password-input" />
+            <button type="button">Entrar</button>
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
