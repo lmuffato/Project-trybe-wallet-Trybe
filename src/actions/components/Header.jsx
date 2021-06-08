1c6882a0bc18963ffc38cc03b698baa46849eef0
Orlando Flores <orlando_flores@id.uff.br>
2021-06-08 17:39:41 -0300

    feat: requisito 5

---
+++ src/actions/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <h1>Trybe Wallet</h1>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = ({ user: { email } }) => ({
+  email,
+});
+
+export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
