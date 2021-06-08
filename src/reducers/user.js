dbb0ca55fa5785cad3eb84afb8c764d61bb99fbc
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-08 14:38:06 -0300

    feat: reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER_LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+function userLoginReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case USER_LOGIN:
+    return action.payload;
+
+  default:
+    return state;
+  }
+}
+
+export default userLoginReducer;
