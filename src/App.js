762b207299efe0010580c672b86cc737d956e00c
AndreBarroso <andre_barroso17@hotmail.com>
2021-06-08 15:13:00 -0300

    Iniciante projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './bootstrap/css/bootstrap.min.css';
+import './App.css';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Route exact path="/" component={ Login } />
+  );
 }
 
 export default App;
