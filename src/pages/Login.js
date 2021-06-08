671cd0452809dd7f2d5dd4790934efb07be0b037
Patrick Morais <ppternunes@gmail.com>
2021-06-08 19:13:37 -0300

    criando login e wallet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+const Login = () => {
+  const [emailInput, setEmailInput] = React.useState('');
+  const [passwordInput, setPasswordInput] = React.useState('');
+  const [login, setLogin] = React.useState(false);
+
+  const dispatch = useDispatch();
+
+  const checkEmail = () => {
+    dispatch({
+      type: 'SET_EMAIL',
+      payload: { email: emailInput },
+    });
+    return (/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(emailInput));
+  };
+
+  const maxLength = 6;
+
+  const checkPass = () => passwordInput.length >= maxLength;
+
+  const handleClick = () => {
+    setLogin(true);
+  };
+
+  return (
+    <div>
+      <input
+        name="email"
+        type="email"
+        data-testid="email-input"
+        value={ emailInput }
+        onChange={ (e) => setEmailInput(e.target.value) }
+      />
+      <input
+        name="password"
+        type="password"
+        data-testid="password-input"
+        minLength="6"
+        onChange={ (e) => setPasswordInput(e.target.value) }
+      />
+      <button
+        type="button"
+        onClick={ handleClick }
+        disabled={ !(checkEmail() && checkPass()) }
+      >
+        Entrar
+      </button>
+      {login && <Redirect to="/carteira" />}
+    </div>
+  );
+};
 
 export default Login;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Header from '../Header';
 
-class Wallet extends React.Component {
-  render() {
-    return <div>TrybeWallet</div>;
-  }
-}
+const Wallet = () => {
+  const email = useSelector((state) => state.user.email);
+
+  return (
+    <>
+      <Header />
+      <h2>
+        bem vindo
+        {email}
+      </h2>
+    </>
+  );
+};
 
 export default Wallet;
