ec56355cf3d109ad2bbb9380280b62a3b692e884
Sergio Martins <sergiomos2010@gmail.com>
2021-06-08 18:10:42 -0300

    Feat: add wallet route

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 export default function Routes() {
   return (
     <Switch>
+      <Route path="/carteira" component={ Wallet } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
