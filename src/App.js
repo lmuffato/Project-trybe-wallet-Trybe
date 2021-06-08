68d367aa6aa150d48737633d5b0f6916855a167e
hheloisa <hhackenhaar@gmail.com>
2021-06-08 19:20:09 -0300

    corrige erro de lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,32 +3,33 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-    <section>
-      <div>Login</div>
-      <label htmlFor="user-email">
-        <input
-          data-testid="email-input"
-          placeholder="nome@email.com"
-          id="user-email"
-          type="email"
-          /* value=
-          onChance= */
+      <section>
+        <div>Login</div>
+        <label htmlFor="user-email">
+          <input
+            data-testid="email-input"
+            placeholder="nome@email.com"
+            id="user-email"
+            type="email"
+            /* value=
+            onChance= */
           />
-      </label>);
+        </label>
 
-      <label htmlFor="user-passw">
-        <input
-          data-testid="password-input"
-          placeholder="nome@email.com"
-          id="user-passw"
-          type="text"
-          /* value=
-          onChance= */
+        <label htmlFor="user-passw">
+          <input
+            data-testid="password-input"
+            placeholder="nome@email.com"
+            id="user-passw"
+            type="text"
+            /* value=
+            onChance= */
           />
-      </label>);
-      <button type="button" /* onCLick= */> Entrar </button>
-    </section>
-    )}
+        </label>
+        <button type="button" /* onCLick= */>Entrar</button>
+      </section>
+    );
+  }
 }
 
 export default Login;
