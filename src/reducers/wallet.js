048d2743cfeea6c7d8f888ddbe7c4b914aa6cdfb
rafael <rafittu@outlook.com>
2021-06-08 16:04:59 -0300

    Iniciando o projeto

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case 'ADICIONAR':
+      return state;
+    case 'EDITAR':
+      return state;
+    case 'EXCLUIR':
+      return state;
+    default:
+      return state;
+  };
+};
+
+export default wallet;
