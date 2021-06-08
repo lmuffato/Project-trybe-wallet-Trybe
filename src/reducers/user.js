7f1d4e9357a93719ad8370854dc5b8414027cd43
Walvesb65 <willian.alvesb65@gmail.com>
2021-06-08 16:03:18 -0300

    Redux-conf

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE_USER = {
+  user: {
+    email: '',
+  },
+};
+
+const userLogin = (state = INITIAL_STATE_USER, action) => {
+  switch (action.type) {
+  case 'action':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default userLogin;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE_WALLET = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const userWallet = (state = INITIAL_STATE_WALLET, action) => {
+  switch (action.type) {
+  case 'action':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default userWallet;
