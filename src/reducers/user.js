1bc25fb331563b93fcfeb1428fd527a1678e392c
lucas-paz <lucas.hpaz@outlook.com>
2021-06-08 15:27:02 -0300

    cria o reducer user e a store

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'NEW_ACTION':
+    return { state: action.state };
+  default:
+    return state;
+  }
+}
+
+export default user;
