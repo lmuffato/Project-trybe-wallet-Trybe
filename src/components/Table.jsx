a6331f8bc6e210e0eda83a665446bdd92382764c
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-08 19:47:52 -0300

    fix: lint

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-
 import elements from '../services/inputs';
 import TBody from './TBody';
 
-const Table = () => {
-  const { expenses } = useSelector((state) => state.wallet);
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          {elements.th.map((th) => (
-            <th key={ th }>{th}</th>
-          ))}
-        </tr>
-      </thead>
-      <TBody />
-    </table>
-  );
-};
+const Table = () => (
+  <table>
+    <thead>
+      <tr>
+        {elements.th.map((th) => (
+          <th key={ th }>{th}</th>
+        ))}
+      </tr>
+    </thead>
+    <TBody />
+  </table>
+);
 
 export default Table;
