f5e2eb908c2672979acd8bfc5a6d5875fc783a8d
Ana Ventura <anaventura1811@gmail.com>
2021-06-08 14:47:19 -0300

    feat: req. 1, configura reducers, provider e store

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,26 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input type="email" id="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            id="password"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {};
+
+function loginReducer(state = initialState, action) {
+  switch (action.type) {
+  case 'LOGIN':
+    return action.value;
+  default:
+    return state;
+  }
+}
+
+export default loginReducer;
