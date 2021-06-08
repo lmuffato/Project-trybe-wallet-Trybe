372e3b83294bd66e60be92650ac7b166b7dd4e0f
jlfagundes <jlfagundes@gmail.com>
2021-06-08 19:10:07 -0300

    feat: (config-reducer) initial structure userReducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'NEW_ACTION':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
