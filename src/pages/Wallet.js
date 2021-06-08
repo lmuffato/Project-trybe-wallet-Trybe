ddf0b774e861122d744a813d2dcd8672302e0c1e
Gui-lira <gui.lira.martins@gmail.com>
2021-06-08 17:53:56 -0300

    5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { user:{ email }} = this.props;
+    return (
+      <div>
+        <header>
+          <h1 data-testid="email-field">{email}</h1>
+          <h1 data-testid="total-field">0</h1>
+          <h1 data-testid="header-currency-field">BRL</h1>
+        </header>
+      </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Wallet.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
