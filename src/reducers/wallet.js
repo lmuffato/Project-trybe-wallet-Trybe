f56afc79b60ea6e56e55f009377ade45add7354e
Maria Baeta <m.martinsdg@gmail.com>
2021-06-08 17:16:41 -0300

    Requisito 1

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// currencies: moedas
+// expenses: despesas
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ADD_EXPENSES':
+    return {
+      ...state,
+      expenses: [...state.expenses, action.value],
+    };
+  default:
+    return state;
+  }
+}
+
+export default wallet;
