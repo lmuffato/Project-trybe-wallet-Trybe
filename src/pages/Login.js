27675d1373ab746468888f31b896e272575d85a3
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-08 14:46:34 -0300

    feat: pagina de login - 1/2/3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+import { userLogin } from '../actions';
+
+import { checkEmail, checkPassword } from '../utils/login';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+  const addUser = () => {
+    const user = {
+      email,
+      password,
+    };
+
+    dispatch(userLogin(user));
+  };
+
+  const checkLogin = () => checkEmail(email) && checkPassword(password);
+
+  return (
+    <div>
+      <input
+        type="email"
+        data-testid="email-input"
+        onChange={ (e) => setEmail(e.target.value) }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        onChange={ (e) => setPassword(e.target.value) }
+      />
+      <button type="button" disabled={ !checkLogin() } onClick={ addUser }>
+        <Link to="/carteira">Entrar</Link>
+      </button>
+    </div>
+  );
+};
 
 export default Login;
