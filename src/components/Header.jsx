5a1ed1aed276e233d52c0e838a012147a25f28e7
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-08 17:30:38 -0400

    correcao requisito 5 - linha 21, componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ class Header extends React.Component {
           </p>
           <p data-testid="total-field">
             Despesa total:
-            { totalValue }
+            { totalValue > 0 ? totalValue : 0 }
           </p>
           <p data-testid="header-currency-field">
             BRL
