dcd51635ff3709c8ba39f94c844f245661842e04
Orlando Flores <orlando_flores@id.uff.br>
2021-06-08 16:35:31 -0300

    feat: requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/wallet" component={ Wallet } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            name="email"
+            required
+            data-testid="email-input"
+            onChange={ this.handleOnChange }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="email">
+          Senha:
+          <input
+            type="password"
+            name="password"
+            required
+            data-testid="password-input"
+            onChange={ this.handleOnChange }
+            value={ password }
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
