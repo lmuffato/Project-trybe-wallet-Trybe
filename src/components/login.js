8ea338c81c01f56e45af74f30bef4490fba329da
Jodiel Briesemeister <jodielll29@gmail.com>
2021-06-08 16:09:19 -0300

    feat: requirement-02

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class login extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validateInput = this.validateInput.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  validateInput() {
+    const { email, password } = this.state;
+    const isValid = email.match(/^\w+@\w+.com$/);
+    const minimumPasswordLength = 5;
+    if (password.length > minimumPasswordLength && isValid) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
     return (
       <div>
@@ -9,13 +39,24 @@ export default class login extends Component {
           type="text"
           data-testid="email-input"
           placeholder="email"
+          name="email"
+          onChange={ this.handleChange }
         />
         <input
           type="text"
           data-testid="password-input"
           placeholder="password"
+          name="password"
+          onChange={ this.handleChange }
         />
-        <button type="button">Entrar</button>
+        <Link to="/carteira">
+          <button
+            disabled={ this.validateInput() }
+            type="button"
+          >
+            Entrar
+          </button>
+        </Link>
       </div>
     );
   }
