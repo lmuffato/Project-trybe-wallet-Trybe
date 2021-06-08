fc283a3560023854f7e90ccb96070dd5443b8669
hheloisa <hhackenhaar@gmail.com>
2021-06-08 18:15:15 -0300

    insere rota para pagina de login, sendo /

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <section>
+      <Switch>
+        <Route path="/" component={ Login } />
+      </Switch>
+    </section>);
 }
 
 export default App;
