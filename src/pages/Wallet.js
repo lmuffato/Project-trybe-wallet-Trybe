cd55a32ce0bc7f8fcfea3ec7e29df6d431382764
Gui-lira <gui.lira.martins@gmail.com>
2021-06-08 18:11:23 -0300

    5 e 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    const { user:{ email }} = this.props;
+    const { user: { email } } = this.props;
     return (
       <div>
         <header>
@@ -12,6 +12,40 @@ class Wallet extends React.Component {
           <h1 data-testid="total-field">0</h1>
           <h1 data-testid="header-currency-field">BRL</h1>
         </header>
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input type="number" />
+          </label>
+          <label htmlFor="descrição">
+            Descrição
+            <input type="text" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select>
+              vazio
+            </select>
+          </label>
+          <label htmlFor="metodo">
+            Método de pagamento
+            <select>
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select>
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>);
   }
 }
