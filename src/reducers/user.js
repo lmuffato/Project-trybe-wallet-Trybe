d4bc15918402fbd17af21e4b1307c245c1287008
Patrick Morais <ppternunes@gmail.com>
2021-06-08 19:06:09 -0300

    criando user e wallet, reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  email: '',
+};
+
+export default function user(state = initialState, action) {
+  switch (action.type) {
+  case 'SET_EMAIL':
+    return { ...state, email: action.payload.email };
+  default:
+    return state;
+  }
+}
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  email: '',
+};
+
+export default function user(state = initialState, action) {
+  switch (action.type) {
+  case 'SET_EMAIL':
+    return { ...state, email: action.payload.email };
+  default:
+    return state;
+  }
+}
