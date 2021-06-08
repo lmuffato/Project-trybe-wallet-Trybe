0e802601cb16e53313641b1dd4eb73195d3d0f91
Walvesb65 <willian.alvesb65@gmail.com>
2021-06-08 16:50:31 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <input type="email" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+      </>
+    );
   }
 }
 
