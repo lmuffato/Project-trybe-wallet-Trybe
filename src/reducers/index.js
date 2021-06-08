e18020657d659a25535e939a35e7aa3e729f7755
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-06-08 15:41:25 -0300

    Start

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,15 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+export default INITIAL_STATE;
