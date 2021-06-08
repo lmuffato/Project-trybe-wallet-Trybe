0977287885f1e50e6a9c56cd13ce687a7e1d69be
guidugaich <guidugaich.dev@gmail.com>
2021-06-08 20:52:14 -0300

    starting userReducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  email: '',
+  password: '',
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
