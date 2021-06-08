a459ea59dbdbf07560461f17ebd0e45f0dc48c95
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-06-08 15:12:07 -0300

    feat: requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <section>
+          <input
+            type="text"
+            placeholder="email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="senha"
+            data-testid="password-input"
+          />
+          <button type="button">
+            Entrar
+          </button>
+        </section>
+      </div>
+    );
   }
 }
 
