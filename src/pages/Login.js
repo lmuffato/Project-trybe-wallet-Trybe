7dba21f4dbb017933a17f06c1dd3c6e937372cd8
Viegas <mvrdgs@gmail.com>
2021-06-08 16:42:01 -0300

    create login page form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,62 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleInput({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit() {
+
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const regex = /^\w+([.-_]?\w+)*@\w+([.-_]?\w+)*(\.\w{2,3})+$/;
+    const passwordLength = 6;
+    return (
+      <form>
+        <label htmlFor="email-input" data-testid="email-input">
+          Email:
+          <input
+            type="text"
+            value={ email }
+            id="email-input"
+            onChange={ this.handleInput }
+            name="email"
+          />
+        </label>
+        <label htmlFor="password-input" data-testid="password-input">
+          Password:
+          <input
+            type="password"
+            value={ password }
+            id="password-input"
+            onChange={ this.handleInput }
+            name="password"
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ this.handleSubmit }
+          disabled={ !email.match(regex) && !password.length < passwordLength }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
