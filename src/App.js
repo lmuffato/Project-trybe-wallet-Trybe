4b4c97af2378508f7e8bbd210b4dbc83ca269861
Rodrigo Rech <rodrigorech92@gmail.com>
2021-06-08 15:06:08 -0300

    Feat: create  routs at App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route path="/wallet" component={ Wallet } />
+      <Route path="/" component={ Login } />
+    </Switch>
+  );
 }
 
 export default App;
