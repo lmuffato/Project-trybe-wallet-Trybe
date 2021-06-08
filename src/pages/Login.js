34074b6a9040baf891a2be7629d02a33e14ddfcf
Felipe Muller <felipeavmuller@gmail.com>
2021-06-08 18:44:58 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <input
+          type="text"
+          placeholder="User"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          data-testid="password-input"
+        />
+        <button type="button">
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
