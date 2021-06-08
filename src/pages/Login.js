cd67fdf7185f841004d1c43edc2fb34dffc1176b
hheloisa <hhackenhaar@gmail.com>
2021-06-08 18:21:38 -0300

    insere inputs de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,8 +3,32 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-    <div>Login</div>);
-  }
+    <section>
+      <div>Login</div>
+      <label htmlFor="user-email">
+        <input
+          data-testid="email-input"
+          placeholder="nome@email.com"
+          id="user-email"
+          type="email"
+          /* value=
+          onChance= */
+          />
+      </label>);
+
+      <label htmlFor="user-passw">
+        <input
+          data-testid="password-input"
+          placeholder="nome@email.com"
+          id="user-passw"
+          type="text"
+          /* value=
+          onChance= */
+          />
+      </label>);
+      
+    </section>
+    )}
 }
 
 export default Login;
