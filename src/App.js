d38cb4c4aa5bc9ef11032fb5c8612629065e1b1b
Eduardo Seije <eduardoseije@gmail.com>
2021-06-08 15:49:38 -0300

    Criando a pág de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router';
+import Login from './pages/Login'
+
+
 
 function App() {
-  return <div>Hello, TrybeWallet!!</div>;
+  return (
+  <>
+    <div>Hello, TrybeWallet!!</div>
+    <Route path="/" component={Login} />
+  </>
+  )
+
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <div>Login</div>
+        <label htmlFor="emailInput">
+          <input
+            id="emailInput"
+            data-testid="email-input"
+            type="email"
+            placeholder="Digite seu e-mail..."
+          />
+        </label>
+        <label htmlFor="passwordInput">
+          <input
+            id="passwordInput"
+            data-testid="password-input"
+            type="password"
+            placeholder="Digite sua senha"
+          />
+        </label>
+        <button id="enterBtn" type="submit">
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
