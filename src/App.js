0c743f5f77359370f02c67c27c373fc73d5e0457
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-08 14:37:19 -0300

    feat: redux base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+
+import './styles/globals.css';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route path="/carteira">
+        <Wallet />
+      </Route>
+
+      <Route path="/">
+        <Login />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Router>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root'),
 );
 
