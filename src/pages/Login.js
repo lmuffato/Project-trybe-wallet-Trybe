0938d949efd199d30c2b51f3b9b8923501a739bb
mallmann02 <leomallmann020302@gmail.com>
2021-06-08 19:19:49 -0300

    feat: User's login page Connection with application's global state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import { login as loginAction } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      shouldRedirect: false
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value
+    })
+  };
+  
+  validateEmail = (email) => {
+    const includesAtSign = email.includes('@');
+    const includesDotCom = email.includes('.com');
+    return includesAtSign && includesDotCom;
+  };
+  
+  submitForm = () => {
+    const { email, password } = this.state;
+    const { login } = this.props;
+    const isPasswdValid = password.length >= 6;
+    if (this.validateEmail(email) && isPasswdValid) {
+      login({email, password});
+      alert('Login efetuado com sucesso');
+      this.setState({
+        email: '',
+        password: '',
+        shouldRedirect: true
+      })
+    } else {
+      alert('Dados inválidos');
+    }
+  };
+  
   render() {
-    return <div>Login</div>;
+    const { email, password, shouldRedirect } = this.state;
+    
+    if (shouldRedirect) {
+      return <Redirect to="/carteira"/>
+    }
+
+    return (
+      <section>
+        <label>
+          E-mail:
+          <input
+            type="text" 
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ (e) => this.handleChange(e)}
+          />
+        </label>
+        
+        <label>
+          Senha:
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ (e) => this.handleChange(e)}
+          />
+        </label>
+
+        <button onClick={ this.submitForm }>
+          Entrar
+        </button>
+      </section>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (e) => dispatch(loginAction(e))
+})
+
+export default connect(null, mapDispatchToProps)(Login);
