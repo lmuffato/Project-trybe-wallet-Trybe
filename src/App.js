92b64f9cdbdae7f076ff44f6d6d4487c68da1d2e
phlopes <phlopes93@gmail.com>
2021-06-08 15:45:28 -0300

    requirement one completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+    </div>);
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <h3>Enter Email and Password</h3>
+        <input type="email" data-testid="email-input" placeholder="Email" />
+        {' '}
+        <br />
+        {' '}
+        <br />
+        <input type="password" data-testid="password-input" placeholder="PassWord" />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
