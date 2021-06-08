a89a9788fb1c0501c3dd1579edce28486776a658
Giovanni <giovannitadeu08@gmail.com>
2021-06-08 17:13:51 -0300

    feat: requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            type="email"
+            value={ email }
+            placeholder="email"
+            data-testid="email-input"
+            onChange={ (event) => this.setState({ email: event.target.value }) }
+          />
+          <input
+            type="password"
+            value={ password }
+            password="senha"
+            data-testid="password-input"
+            onChange={ (event) => this.setState({ password: event.target.value }) }
+          />
+        </div>
+        <Link
+          to="/carteira"
+        >
+          <button type="button">Entrar</button>
+        </Link>
+      </div>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+function user(state = INITIAL_STATE, { type, payload }) {
+  switch (type) {
+  case 'LOGIN':
+    return { email: payload };
+  default:
+    return state;
+  }
+}
+
+export default user;
