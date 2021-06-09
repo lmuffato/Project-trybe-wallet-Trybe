c71549562c9e3b003ae4e1a8112ba4d8160e29ca
Elisa <elisafranca.carvalho@gmail.com>
2021-06-09 11:10:51 -0300

    fix: req 6

--- src/pages/components/ExpensesAddForm.js
+++ src/pages/components/ExpensesAddForm.js
@@ -4,39 +4,39 @@ export default class ExpensesAddForm extends Component {
   render() {
     return (
       <form>
-        <label htmlFor="valor">
+        <label htmlFor="valor" id="valor">
           Valor
-          <input type="text" name="valor" />
+          <input type="text" name="valor" aria-labelledby="valor" />
         </label>
-        <label htmlFor="descrição">
+        <label htmlFor="descrição" id="descrição">
           Descrição
-          <input type="text" name="descrição" />
+          <textarea type="text" name="descrição" aria-labelledby="descrição" />
         </label>
-        <label htmlFor="moeda">
+        <label htmlFor="moeda" id="moeda">
           Moeda
           <select
             name="moeda"
-            id="moeda"
+            aria-labelledby="moeda"
           >
             <option value="Vazio">Vazio</option>
           </select>
         </label>
-        <label htmlFor="paymentType">
+        <label htmlFor="paymentType" id="paymentType">
           Método de pagamento
           <select
             name="paymentType"
-            id="paymentType"
+            aria-labelledby="paymentType"
           >
             <option value="Dinheiro">Dinheiro</option>
             <option value="Cartão de crédito">Cartão de crédito</option>
             <option value="Cartão de débito">Cartão de débito</option>
           </select>
         </label>
-        <label htmlFor="tag">
+        <label htmlFor="tag" id="tag">
           Tag
           <select
             name="tag"
-            id="tag"
+            aria-labelledby="tag"
           >
             <option value="Alimentação">Alimentação</option>
             <option value="Lazer">Lazer</option>
