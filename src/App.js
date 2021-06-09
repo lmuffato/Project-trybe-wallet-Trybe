ffd220b5f683a6cb492eac15eb4cf02097006565
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-06-09 15:39:06 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <h1>Header</h1>
+        <span data-testid="email-field">{ email }</span>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
