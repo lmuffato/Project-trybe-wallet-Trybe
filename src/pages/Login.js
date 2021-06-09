89c76030acf22eb792fea1c27605f2f6ced1ca97
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-06-09 18:01:31 -0300

    Requisito 1: Login Page - done itgit status

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { login } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            data-testid="email-input"
+            type="text"
+            onChange={ (v) => this.setState({ email: v.target.value }) }
+            placeholder="email"
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            onChange={ (v) => this.setState({ senha: v.target.value }) }
+            placeholder="senha"
+          />
+        </div>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (v) => dispatch(login(v)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,17 @@
 // import user from './user';
 // import wallet from './wallet';
+import { combineReducers } from 'redux';
+import userReducer from './user';
+import walletReducer from './wallet';
+import loginReducer from './login';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const rootReducer = combineReducers({
+  loginReducer,
+  userReducer,
+  walletReducer,
+});
+
+export default rootReducer;
