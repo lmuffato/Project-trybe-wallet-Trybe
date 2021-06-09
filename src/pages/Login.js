bd6ac783bcbdba7fbe8ebed5a6cd1f770c58b6b3
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-06-08 22:41:44 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  validate() {
+    const { email, password } = this.state;
+    const minLength = 6;
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    if ((password.length >= minLength) && (emailPattern.test(email) === true)) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
-      <form>
+      <form className="form-login">
         <input
           type="email"
           placeholder="Email"
           data-testid="email-input"
+          onChange={ (e) => this.setState({ email: e.target.value }) }
+          value={ email }
           required
         />
         <input
@@ -16,11 +38,14 @@ class Login extends React.Component {
           placeholder="Senha"
           minLength="6"
           data-testid="password-input"
+          onChange={ (e) => this.setState({ password: e.target.value }) }
+          value={ password }
           required
         />
         <Link to="/carteira">
           <button
             type="button"
+            disabled={ this.validate() }
           >
             Entrar
           </button>
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,4 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+// const INITIAL_STATE = {
+//   email: '',
+// };
