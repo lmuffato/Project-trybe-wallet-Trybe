d861743f2a93925dd4cd5370547abac4129ae3a3
Felipe Flores <felipefloresvendas@gmail.com>
2021-06-09 07:51:12 -0300

    feat: Requirement 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -7,12 +7,14 @@ class Wallet extends React.Component {
   render() {
     const { userAddExpense, userAddCurrencie } = this.props;
     const { email, isLogged } = this.props;
+    const expend = 0;
     return (
       <header>
         <h2>Trybe Wallet</h2>
-        <p data-testid="email-field">{isLogged ? email : 'nenhum email'}</p>
-        <p data-testid="total-field">Despesas Totais : (pegar despesas)</p>
-        <p data-testid="header-currency-field">Pegar qual câmbio está sendo utilizado</p>
+        <p data-testid="email-field">{email}</p>
+        <p>Despesas Totais: </p>
+        <p data-testid="total-field">{expend}</p>
+        <p data-testid="header-currency-field">BRL</p>
       </header>
     );
   }
@@ -23,14 +25,13 @@ const secondMapDispatchToProps = (dispatch) => ({
   userAddCurrencie: () => dispatch(walletAddCurrencie()),
 });
 
-const secondMapStateToProps = ({ wallet: { currencies, expenses } },
-  { user: { email, isLogged } }) => ({
+const secondMapStateToProps = ({ wallet: { currencies, expenses },
+  user: { email, isLogged } }) => ({
   currencies,
   expenses,
   email,
   isLogged,
 });
-console.log(secondMapStateToProps.expenses);
 
 Wallet.propTypes = {
   userAddExpense: PropTypes.func.isRequired,
