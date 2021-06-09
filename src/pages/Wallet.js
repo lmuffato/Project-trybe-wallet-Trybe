b3af276b60cd0c5531c0c2d73fb34bdd1b895771
Giovanni <giovannitadeu08@gmail.com>
2021-06-09 16:34:09 -0300

    feat: requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+        <div>
+          TrybeWallet
+        </div>
+      </div>
+    );
   }
 }
 
