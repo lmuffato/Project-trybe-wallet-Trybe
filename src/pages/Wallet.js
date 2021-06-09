066189d534bf3d63c7229ff3a0bfb0f9daf3d1ca
AndreBarroso <andre_barroso17@hotmail.com>
2021-06-09 09:36:14 -0300

    feat: requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.sumExpenses = this.sumExpenses.bind(this);
+    this.choiceCurrency = this.choiceCurrency.bind(this);
+  }
+
+  sumExpenses() {
+    let totalExpenses = 0;
+    const { arrayExpenses } = this.props;
+    if (arrayExpenses.length === 0) totalExpenses = 0;
+    else totalExpenses = arrayExpenses.reduce((acc, cur) => acc + cur);
+    return totalExpenses;
+  }
+
+  choiceCurrency() {
+    let currentCurrency = 'BRL';
+    const { arrayCurrencys } = this.props;
+    if (arrayCurrencys.length === 0) currentCurrency = 'BRL';
+    /* suficiente para o requisito 5,
+      mas deverá ser completado nos próximos requisitos */
+
+    return currentCurrency;
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { receiveLoginEmail } = this.props;
+    return (
+      <header>
+        <div data-testid="email-field">{receiveLoginEmail}</div>
+        <div data-testid="total-field">{ this.sumExpenses() }</div>
+
+        {/* suficiente para o requisito 5, mas deverá ser
+           completado nos próximos requisitos */}
+        <div data-testid="header-currency-field">{this.choiceCurrency()}</div>
+
+      </header>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  receiveLoginEmail: state.user.email,
+  arrayExpenses: state.wallet.expenses,
+  arrayCurrencys: state.wallet.currencies,
+});
+
+Wallet.propTypes = {
+  receiveLoginEmail: PropTypes.func.isRequired,
+  arrayExpenses: PropTypes.func.isRequired,
+  arrayCurrencys: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+// import { VALID_EMAIL } from '../actions/index';
+
+const initialState = {
+  currencies: ['BRL'],
+  expenses: [],
+};
+
+function wallet(state = initialState, action) {
+  switch (action.type) {
+  // case VALID_EMAIL:
+  // return { ...state, user: { ...user.email, email: action.email },
+  //  };
+  default:
+    return state;
+  }
+}
+
+export default wallet;
