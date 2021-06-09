b06fddcbe2c5fe99fd613df858496a9f79df2393
guilhermemd <guilherme.md@gmail.com>
2021-06-09 18:18:24 -0300

    redux DONE

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER } from '../actions/index';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case USER:
+    return { ...state, action };
+  default:
+    return { ...state };
+  }
+};
+
+export default user;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET } from '../actions/index';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET:
+    return { ...state, action };
+  default:
+    return { ...state };
+  }
+};
+
+export default wallet;
