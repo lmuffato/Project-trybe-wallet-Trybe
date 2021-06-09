4d2a538a5bcdc7c631f6db1c84ac661a83fb76d9
MaxRudim <max.hermsdorff@gmail.com>
2021-06-09 18:24:31 -0300

    Req - 02 - AGORA VAII

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEmailAndPassword = this.handleEmailAndPassword.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  // Ref: validação de e-mail: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
+  handleEmailAndPassword() {
+    const { email, password } = this.state;
+    const re = /\S+@\S+\.\S+/;
+    const verifyEmail = re.test(email);
+    const passLength = 5;
+    if (password.length >= passLength && verifyEmail) {
+      this.setState({ isButtonDisabled: false });
+    } else {
+      this.setState({ isButtonDisabled: true });
+    }
+  }
+
+  handleChange(event) {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+    this.handleEmailAndPassword();
+  }
+
   render() {
+    const { email, password, isButtonDisabled } = this.state;
     return (
       <>
         <h1>Trybe Wallet</h1>
@@ -10,22 +43,28 @@ class Login extends React.Component {
             type="email"
             placeholder="digite seu e-mail neste campo"
             data-testid="email-input"
-            // value={ email }
-            // onChange
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
           />
           <input
             type="password"
             data-testid="password-input"
             placeholder="digte sua senha neste campo"
-            // value={ password }
-            // onChange
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
           />
-          <button
-            type="button"
-          // onClick
-          >
-            Entrar
-          </button>
+          <Link to="/carteira">
+            <button
+              type="button"
+              // Ref: desabiitando botão: https://qastack.com.br/programming/3014649/how-to-disable-html-button-using-javascript
+              disabled={ isButtonDisabled }
+              onClick={ this.handleLogin }
+            >
+              Entrar
+            </button>
+          </Link>
         </form>
       </>
     );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './user';
-// import wallet from './wallet';
+import wallet from './wallet';
 
 const rootReducers = combineReducers({
   user,
-  // wallet,
+  wallet,
 });
 
 export default rootReducers;
