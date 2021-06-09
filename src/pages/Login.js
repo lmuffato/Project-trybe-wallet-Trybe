212ff55c03d7316c2f2a646e2f09033fd17856d1
Villefor <franklincrius@gmail.com>
2021-06-09 03:13:50 -0300

    tentativa de 2 requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,58 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    const button = document.querySelector('#loginButton');
+    const lengthNumber = 6;
+    if (!value.includes('.com') && value.length >= lengthNumber) {
+      button.disabled = true;
+    }
+    if (value.includes('.com') && value.length >= lengthNumber) {
+      button.disabled = false;
+    }
+    if (value.includes('.com') && value.length < lengthNumber) {
+      button.disabled = true;
+    }
+  }
+
   render() {
     return (
       <section>
         <form>
           <label htmlFor="Email">
+            {' '}
+            Email
             <input
               data-testid="email-input"
-              id="email-input"
+              id="Email"
               placeholder="Insert e-mail"
-              type="text"
+              type="email"
+              onChange={ this.handleChange }
               required
             />
           </label>
-
           <label htmlFor="Password">
+            {' '}
+            Password
             <input
               data-testid="password-input"
-              id="password-input"
+              id="Password"
               placeholder="Insert password"
               type="text"
+              min="6"
+              onChange={ this.handleChange }
               required
             />
           </label>
+          <button type="submit" id="loginButton" disabled>
+            Entrar
+          </button>
         </form>
-        <button type="submit">
-          Entrar
-        </button>
       </section>
     );
   }
