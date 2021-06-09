724340aa2885fce406e61bfa88a2649ff0a4e089
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-06-09 18:37:28 -0300

    feat: requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,52 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      senha: '',
+      disabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+    const { email, senha } = this.state;
+    const result = /\w+@\w+\.\w+/gi.test(email);
+    const resultSenha = /(.{5,})/gi.test(senha);
+    if (result && resultSenha) {
+      this.setState({ disabled: false });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div>
         <form>
           <input
+            name="email"
             type="email"
-            placeholder="Loguin"
+            placeholder="Login"
             data-testid="email-input"
+            onChange={ this.handleChange }
           />
           <input
+            name="senha"
             type="password"
             placeholder="Senha"
             data-testid="password-input"
+            onChange={ this.handleChange }
           />
-          <button type="submit">Entrar</button>
+          <button type="button" disabled={ disabled }>
+            Entrar
+          </button>
         </form>
       </div>
     );
