6d3a6547b0170a2db35d7889f05eb6dfeefd5f62
rafael <rafittu@outlook.com>
2021-06-08 21:32:26 -0300

    req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,27 +7,49 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      logginDisabled: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.emailValidate = this.emailValidate.bind(this);
+    this.passwordValidate = this.passwordValidate.bind(this);
+  }
+
+  // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+  emailValidate() {
+    const { email } = this.state;
+    const regExp = /^\w+@\w+.com$/;
+
+    return regExp.test(email);
+  }
+
+  passwordValidate() {
+    const { password } = this.state;
+    const passwordMinimumLength = 6;
+
+    return password.length >= passwordMinimumLength;
   }
 
   handleChange({ target }) {
     const { name, value } = target;
 
-    this.setState({
-      [name]: value,
+    this.setState({ [name]: value }, () => {
+      if (this.emailValidate() && this.passwordValidate()) {
+        this.setState({ logginDisabled: false });
+      } else {
+        this.setState({ logginDisabled: true });
+      }
     });
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, logginDisabled } = this.state;
     return (
       <form>
         <label htmlFor="email">
           Email:
           <input
-            type="text"
+            type="email"
             name="email"
             data-testid="email-input"
             value={ email }
@@ -47,7 +69,12 @@ class Login extends React.Component {
         </label>
 
         <Link to="/carteira">
-          <button type="button">Entrar</button>
+          <button
+            type="button"
+            disabled={ logginDisabled }
+          >
+            Entrar
+          </button>
         </Link>
       </form>
     );
