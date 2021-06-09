ca6ebe6d22e5fabc3b2d29789fa27064bead80f6
igson <igson660@gmail.com>
2021-06-08 22:23:32 -0500

    quesito 01 completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,46 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ (e) => console.log(e.target.value) }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ (e) => console.log(e.target.value) }
+          />
+        </label>
+        <button
+          type="submit"
+          disabled
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
