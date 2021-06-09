bc5322da3ae477c1cc5b4ed957a738410e85494c
Orlando Flores <orlando_flores@id.uff.br>
2021-06-09 19:01:44 -0300

    feat: requisito 9

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Expense extends React.Component {
   render() {
-    // const { description, value } = this.props;
+    const {
+      description,
+      value,
+      tag,
+      method,
+      currency,
+      exchangeRates,
+    } = this.props;
+    const currencyName = exchangeRates[currency].name.split('/')[0];
+    const exchangeRateUsed = exchangeRates[currency].ask;
+    const exchangeRateUsedFloat = parseFloat(exchangeRateUsed).toFixed(2);
+    const valueInBRL = parseFloat(exchangeRateUsed * value).toFixed(2);
     return (
-      <>
-        {/* <h1>{ description }</h1> */}
-        {/* <h2>{ value }</h2> */}
-      </>
+      <tr>
+        <td>{description}</td>
+        <td>{tag}</td>
+        <td>{method}</td>
+        <td>{value}</td>
+        <td>{currencyName}</td>
+        <td>{exchangeRateUsedFloat}</td>
+        <td>{valueInBRL}</td>
+        <td>Real</td>
+      </tr>
+
     );
   }
 }
 
 export default (Expense);
+
+Expense.propTypes = {
+  description: PropTypes.string,
+  value: PropTypes.number,
+  tag: PropTypes.string,
+  method: PropTypes.string,
+  currency: PropTypes.string,
+  exchangeRates: PropTypes.object,
+}.isRequired;
--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -7,9 +7,24 @@ class ExpensesList extends React.Component {
   render() {
     const { expenses } = this.props;
     return (
-      <>
-        {expenses.map((expense) => <Expense key={ expense.id } { ...expense } />)}
-      </>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => <Expense key={ expense.id } { ...expense } />)}
+        </tbody>
+      </table>
     );
   }
 }
