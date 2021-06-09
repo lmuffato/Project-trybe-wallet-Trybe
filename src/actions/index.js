03737b9783b25ef5b5c4599a547ea4fec9ed897b
rcalvs <rafaelcalvette@gmail.com>
2021-06-09 16:23:29 -0300

    Wallet page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
-// Coloque aqui suas actions
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+
+export function saveEmail(email) {
+  return { type: SAVE_EMAIL, email };
+}
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { string } from 'prop-types';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
-  }
+    const { email } = this.props;
+    return (
+      <div>
+        <Header
+          email={ email }
+        />
+      </div>
+    );  }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: string,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Wallet);
