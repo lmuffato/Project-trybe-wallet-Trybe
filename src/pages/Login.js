6a1e0a852701b0713d2eb17756b31c60e9ee567a
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-06-09 07:58:04 -0300

    req 1 done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
+export default class Login extends Component {
+  constructor(){
+    super();
+    this.state = {
+        email: '',
+        password: '',
+      };
+    }
+
+
+  updateState = (field, event) => {
+    const { value } = event.target;
+    this.setState({ [field]: value });
   }
-}
 
-export default Login;
+  renderEmailInput = () => {
+    return(
+      <label htmlFor="emailInput" >
+        Email:
+        <input
+          type="email"
+          id="emailInput"
+          placeholder="User Email"
+          data-testid="email-input"
+          onChange={ (event) => this.updateState('email', event) }
+        />
+      </label>
+    );
+  }
+
+  renderPasswordInput = () => {
+    return(
+      <label htmlFor="passWordInput" >
+        PassWord:
+        <input
+          type="password"
+          id="passWordInput"
+          placeholder="User Password"
+          onChange={ (event) => this.updateState('password', event) }
+          data-testid="password-input"
+        />
+      </label>
+    );
+  }
+
+  renderEntryButton = () => {
+    return(
+      <button
+        type="button"
+      >
+        Entrar
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        { this.renderEmailInput() }
+        { this.renderPasswordInput() }
+        { this.renderEntryButton() }
+      </div>
+    );
+  };
+};
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN_EMAIL } from '../actions';
+
+const INIT_STATE = { email: '' };
+
+const userReducer = (state = INIT_STATE, action) => {
+  switch(action.type) {
+    case LOGIN_EMAIL: {
+      return { email: action.email };
+    };
+    default: return state;
+  };
+};
+
+export default userReducer;
