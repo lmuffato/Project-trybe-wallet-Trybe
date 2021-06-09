0661c6621cf69191067992d470802ce528668ba6
rogerio <rogerio.cl@gmail.com>
2021-06-09 18:52:20 -0300

    requisto 6 e acho q o 7 tb

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,8 +4,92 @@ import { connect } from 'react-redux';
 import { fetchCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentExpense: {
+        value: 0,
+        description: 'teste',
+        currency: 'BRL',
+        paymentMode: 'Dinheiro',
+        tag: '',
+      },
+    };
+    this.formToInserExpense = this.formToInserExpense.bind(this);
+    this.handleExpense = this.handleExpense.bind(this);
+    this.generateInput = this.generateInput.bind(this);
+    this.generateSelect = this.generateSelect.bind(this);
+  }
+
+  componentDidMount() {
+    const { updateCurrencies } = this.props;
+    updateCurrencies();
+  }
+
+  handleExpense({ target: { name, value } }) {
+    this.setState((previusState) => ({
+      currentExpense: { ...previusState.currentExpense, [name]: value },
+    }));
+  }
+
+  generateSelect(label, name, options, value) {
+    return (
+      <label htmlFor={ name }>
+        {label}
+        <select
+          name={ name }
+          value={ value }
+          onChange={ this.handleExpense }
+        >
+          {options.map((option, index) => (
+            <option key={ index }>{option}</option>
+          ))}
+        </select>
+      </label>
+    );
+  }
+
+  generateInput(label, name, type, value) {
+    return (
+      <label htmlFor={ name }>
+        {label}
+        <input
+          name={ name }
+          type={ type }
+          value={ value }
+          onChange={ this.handleExpense }
+        />
+      </label>
+    );
+  }
+
+  formToInserExpense() {
+    const {
+      currentExpense: { value, description, currency, paymentMode, tag },
+    } = this.state;
+    const tagOptions = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+    const paymentOptions = ['dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    const { currencies } = this.props;
+    const currenciesList = Object.keys(currencies).filter((type) => type !== 'USDT');
+    const { generateInput, generateSelect } = this;
+    return (
+      <form>
+        {generateInput('Valor:', 'value', 'number', value)}
+        {generateInput('Descrição:', 'description', 'text', description)}
+        {generateSelect('Moeda:', 'currency', currenciesList, currency)}
+        {generateSelect(
+          'Método de pagamento:',
+          'paymentMode',
+          paymentOptions,
+          paymentMode,
+        )}
+        {generateSelect('Tag:', 'tag', tagOptions, tag)}
+      </form>
+    );
+  }
+
   render() {
-    const { email } = this.props;
+    const { email, isLoalding } = this.props;
     return (
       <main>
         <header>
@@ -21,7 +105,13 @@ class Wallet extends React.Component {
               <span data-testid="total-field">0</span>
             </div>
           </section>
+          {
+            isLoalding
+              ? <h1>Carregado</h1>
+              : <h1>Insira sua depesa</h1>
+          }
         </header>
+        {this.formToInserExpense()}
       </main>
     );
   }
@@ -29,6 +119,9 @@ class Wallet extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  currencies: state.wallet.currencies,
+  expenses: state.wallet.expenses,
+  isLoalding: state.wallet.isLoalding,
 });
 
 const mapDispachToProps = (dispach) => ({
@@ -37,6 +130,10 @@ const mapDispachToProps = (dispach) => ({
 
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
+  updateCurrencies: PropTypes.func.isRequired,
+  currencies: PropTypes.objectOf(PropTypes.shape(PropTypes.object)).isRequired,
+  // expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isLoalding: PropTypes.bool.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispachToProps)(Wallet);
