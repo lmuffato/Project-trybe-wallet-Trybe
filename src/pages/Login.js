87a59bf8ad055130c4f548878fb99ef2f5665696
Gabriel Ferreira <glucas709@gmail.com>
2021-06-09 17:48:52 -0300

    fix bootstrap error

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
 
 class Login extends React.Component {
   render() {
     return (
       <section className="login-page">
-        <Form className="login-form-container">
-          <Form.Label className="login-title">Login</Form.Label>
-          <Form.Group className="login-input-container">
-            <Form.Control
+        <form className="login-form-container">
+          <span className="login-title">Login</span>
+          <div className="login-input-container">
+            <input
               type="email"
               placeholder="E-mail"
               data-testid="email-input"
+              className="form-control"
             />
-            <Form.Control
+            <input
               type="password"
               placeholder="Password"
               data-testid="password-input"
+              className="form-control"
             />
-          </Form.Group>
-          <Button type="submit" className="login-button">
+          </div>
+          <button type="submit" className="btn login-button">
             Entrar
-          </Button>
-        </Form>
+          </button>
+        </form>
       </section>
     );
   }
