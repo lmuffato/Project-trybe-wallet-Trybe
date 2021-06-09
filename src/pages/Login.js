c40b2997b5c93c5608603182b730d54dbe7bd59c
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-06-09 12:58:37 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,50 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            onChange={ this.handleLogin }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          <input
+            type="text"
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleLogin }
+            value={ password }
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
+import { combineReducers } from 'redux';
 // import user from './user';
+import user from './user';
 // import wallet from './wallet';
 
+const reducer = combineReducers({ user });
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+export default reducer;
