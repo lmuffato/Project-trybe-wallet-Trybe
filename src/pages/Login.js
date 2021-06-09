67e0e8b46a9e7dfc6c947512d68026c568ffcb5f
Marcelo Maurício <mmauricio.design@gmail.com>
2021-06-09 09:52:15 -0300

    feat: create login screen - requirement 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <form>
+          <input type="email" data-testid="email-input" placeholder="alguem@email.com" />
+          <input type="password" data-testid="password-input" />
+          <button type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
