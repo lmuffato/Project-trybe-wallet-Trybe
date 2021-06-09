5afce44e8f35ac61327bf6e56841f2d938a790ee
MaxRudim <max.hermsdorff@gmail.com>
2021-06-08 21:43:31 -0300

    Req. 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <h1>Trybe Wallet</h1>
+        <form>
+          <input
+            type="email"
+            placeholder="digite seu e-mail neste campo"
+            data-testid="email-input"
+            // value={ email }
+            // onChange
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="digte sua senha neste campo"
+            // value={ password }
+            // onChange
+          />
+          <button
+            type="button"
+          // onClick
+          >
+            Entrar
+          </button>
+        </form>
+      </>
+    );
   }
 }
 
