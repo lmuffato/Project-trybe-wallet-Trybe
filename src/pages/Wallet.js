2b39afe91877ed229b880d098df4234d4a55dacc
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-06-09 18:43:19 -0300

    Desafio 5 OK

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import Header from './Header';
 
 class Wallet extends React.Component {
@@ -46,12 +44,4 @@ class Wallet extends React.Component {
   }
 }
 
-// const mapStateToProps = (state) => ({
-//   userEmail: state.user.email,
-// });
-
-// Wallet.propTypes = {
-//   userEmail: PropTypes.string.isRequired,
-// };
-
-export default connect(mapStateToProps, null)(Wallet);
+export default Wallet;
