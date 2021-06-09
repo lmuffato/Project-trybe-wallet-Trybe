a19d957c0fdd7f5670cb9baa410bb907dba7a26d
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-09 01:06:57 -0300

    save email Store and redirect /carteira - requirement 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,5 @@
 // Coloque aqui suas actions
-const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_EMAIL = 'SAVE_EMAIL';
 
 export const saveEmail = (email) => (
   { type: SAVE_EMAIL, email });
-
-export default SAVE_EMAIL;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
 import React, { useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { saveEmail } from '../actions';
 
 const isValid = (emailValue, passwordValue) => {
   const check = /(^\D\w+)@\w+(\.)\w+/gi.test(emailValue)
@@ -19,6 +22,8 @@ function Login() {
   const inputEmail = useRef(null);
   const [userEmail, setUserEmail] = useState({ email: '' });
   const [btnDisabled, setBtnDisabled] = useState({ disabled: true });
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleChange = ({ target: { name, value } }, valitation) => {
     const email = inputEmail.current.value;
@@ -27,6 +32,11 @@ function Login() {
       setUserEmail({ [name]: value });
     }
     valitation(email, password, setBtnDisabled);
+  };
+
+  const handleSubmit = () => {
+    dispatch((saveEmail(userEmail.email)));
+    history.push('/carteira');
   };
 
   return (
@@ -48,10 +58,7 @@ function Login() {
         onChange={ (event) => handleChange(event, checkPassEmail) }
         ref={ inputPassword }
       />
-      <button
-        type="button"
-        disabled={ btnDisabled.disabled }
-      >
+      <button type="button" disabled={ btnDisabled.disabled } onClick={ handleSubmit }>
         Entrar
       </button>
     </div>
