32d4d5570ec14843654cc4b7631f68d4d901d901
Marcelo Maurício <mmauricio.design@gmail.com>
2021-06-09 16:50:53 -0300

    feat: created a form above Header info - requirement 06

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,51 @@ class Header extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <div>
-        <span data-testid="email-field">{`Email: ${email}`}</span>
-        <span data-testid="total-field">Despesa Total: R$ 0.00</span>
-        <span data-testid="header-currency-field">BRL</span>
-      </div>
+      <>
+        <div>
+          <span data-testid="email-field">{`Email: ${email}`}</span>
+          <span data-testid="total-field">Despesa Total: R$ 0.00</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </div>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input type="number" id="value" />
+          </label>
+
+          <label htmlFor="description">
+            Descrição:
+            <input type="text" id="description" />
+          </label>
+
+          <label htmlFor="currencies">
+            Moeda:
+            <select id="currencies">
+              <option value="0">0</option>
+            </select>
+          </label>
+
+          <label htmlFor="payment">
+            Método de pagamento
+            <select id="payment">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
