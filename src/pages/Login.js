77bf826699b7cb516a1dbaa30dd0ee8eea2a1d01
Elisa <elisafranca.carvalho@gmail.com>
2021-06-08 22:22:20 -0300

    feat: implement req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,68 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disableButton: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validEmail = this.validEmail.bind(this);
+    this.validPassword = this.validPassword.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+    if (this.validEmail() && this.validPassword() === true) {
+      this.setState({
+        disableButton: false,
+      });
+    }
+    console.log(this.validEmail());
+  }
+
+  validEmail() {
+    const { email } = this.state;
+    // http://www.regexplained.co.uk/
+    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+    // https://regexr.com/
+    const regex = /^\w+@\w+\.com$/;
+    return regex.test(email);
+  }
+
+  validPassword() {
+    const { password } = this.state;
+    const rightLength = 5;
+    if (password.length >= rightLength) {
+      return true;
+    }
+    return false;
+  }
+
   render() {
+    const { disableButton } = this.state;
     return (
       <div>
-        <input data-testid="email-input" placeholder="e-mail" />
-        <input data-testid="password-input" placeholder="senha" />
-        <button type="submit">Entrar</button>
+        <input
+          name="email"
+          type="text"
+          data-testid="email-input"
+          placeholder="name@email.com"
+          onChange={ this.handleChange }
+        />
+        <input
+          name="password"
+          data-testid="password-input"
+          placeholder="senha"
+          type="password"
+          onChange={ this.handleChange }
+        />
+        <button type="submit" disabled={ disableButton }>Entrar</button>
       </div>
     );
   }
