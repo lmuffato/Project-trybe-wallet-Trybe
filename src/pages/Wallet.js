3bfaec6e01f35fb6fc0f7813574be695e7f651d2
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-06-09 18:01:16 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,11 +6,47 @@ class Wallet extends React.Component {
   render() {
     const { user } = this.props;
     return (
-      <header>
-        <p data-testid="header-currency-field">Câmbio: BRL</p>
-        <p data-testid="total-field">Valor total: 0</p>
-        <h6 data-testid="email-field">{user}</h6>
-      </header>
+      <>
+        <header>
+          <p data-testid="header-currency-field">Câmbio: BRL</p>
+          <p data-testid="total-field">Valor total: 0</p>
+          <h6 data-testid="email-field">{user}</h6>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input type="text" name="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input type="text" name="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda:
+            <select name="currency">
+              <option value="BRL">BRL</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento:
+            <select name="payment">
+              <option value="cash">Dinheiro</option>
+              <option value="credit">Cartão de crédito</option>
+              <option value="debit">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="type">
+            Tag
+            <select name="type">
+              <option value="food">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transportation">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
