6b4e6234fa9a3c7f6731d05a12d0167f3f971302
defauth98 <neto.daniribeiro@gmail.com>
2021-06-09 14:51:33 -0300

    configuracao inicial do redux

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: ''
+}
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.key) {
+    default:
+      return state;
+  }
+}
+
+export default user;
