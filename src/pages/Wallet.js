cd43333ef829c682aef7f1bc7b3537d2bef41868
AndreBarroso <andre_barroso17@hotmail.com>
2021-06-09 10:55:52 -0300

    feat: transforma header em componente

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,44 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Header from '../componentes/Header';
 
 class Wallet extends React.Component {
-  constructor() {
-    super();
-    this.sumExpenses = this.sumExpenses.bind(this);
-    this.choiceCurrency = this.choiceCurrency.bind(this);
-  }
-
-  sumExpenses() {
-    let totalExpenses = 0;
-    const { arrayExpenses } = this.props;
-    if (arrayExpenses.length === 0) totalExpenses = 0;
-    else totalExpenses = arrayExpenses.reduce((acc, cur) => acc + cur);
-    return totalExpenses;
-  }
-
-  choiceCurrency() {
-    let currentCurrency = 'BRL';
-    const { arrayCurrencys } = this.props;
-    if (arrayCurrencys.length === 0) currentCurrency = 'BRL';
-    /* suficiente para o requisito 5,
-      mas deverá ser completado nos próximos requisitos */
-
-    return currentCurrency;
-  }
-
   render() {
-    const { receiveLoginEmail } = this.props;
+    const { receiveLoginEmail, arrayExpenses, arrayCurrencys } = this.props;
     return (
-      <header>
-        <div data-testid="email-field">{receiveLoginEmail}</div>
-        <div data-testid="total-field">{ this.sumExpenses() }</div>
-
-        {/* suficiente para o requisito 5, mas deverá ser
-           completado nos próximos requisitos */}
-        <div data-testid="header-currency-field">{this.choiceCurrency()}</div>
-
-      </header>);
+      <div>
+        <Header
+          arrayExpenses={ arrayExpenses }
+          arrayCurrencys={ arrayCurrencys }
+          receiveLoginEmail={ receiveLoginEmail }
+        />
+      </div>
+    );
   }
 }
 
