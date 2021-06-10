01d4a3805bc3c1404d6b3b5c8d5681955931975e
hheloisa <hhackenhaar@gmail.com>
2021-06-09 21:50:29 -0300

    Resolve requisito 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+// Referência para resolver req 5, {referente ao initialValue} a partir do trabalho do Anderson Silva: https://github.com/tryber/sd-010-a-project-trybewallet/pull/75/files
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      initialValue: 0,
+    };
+  }
+
   render() {
     const { email } = this.props;
+    const { initialValue } = this.state;
     // totalValue
     return (
       <div className="HeaderClass">
         <div data-testid="email-field">{ email }</div>
         <div data-testid="total-field">
           Despesa total:
+          {initialValue}
         </div>
         <div data-testid="header-currency-field">BRL</div>
       </div>
