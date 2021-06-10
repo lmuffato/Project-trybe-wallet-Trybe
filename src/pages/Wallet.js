94340d68666507be9c93d2f1e888cff25bfc80a4
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-06-10 15:12:59 -0300

    :rocket: Requisito 09

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Form from '../components/Form';
 import Header from '../components/Header';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
-        <Form />
         <Header />
+        <Form />
+        <Table />
       </div>
     );
   }
