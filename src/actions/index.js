9eea5c1a0af34fb25698b98511ac13d16bd7d513
Eduardo Seije <eduardoseije@gmail.com>
2021-06-10 19:34:46 -0300

    Requisito 07 - correção bug requisito 03

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,7 @@
-import fetchAPI from '../services/requestApi';
-
 // Coloque aqui suas actions
 const userAction = (payload) => ({
   type: 'TYPE_EMAIL',
   payload,
 });
 
-const updateCurrency = () => async (dispatch) => {
-  const currencies = await fetchAPI();
-  dispatch({ type: 'UPDATE_CURRENCY',
-    currencies });
-};
-
-export default { userAction, updateCurrency };
+export default userAction;
