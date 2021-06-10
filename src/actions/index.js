522df3e6ff965422eaf37c58382a45975f30d29b
jlfagundes <jlfagundes@gmail.com>
2021-06-10 16:18:25 -0300

    feat: (config-actions) defined intial actions done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,9 @@
 // Coloque aqui suas actions
-const ADD_USER = {
-    type:
-    payload:
-}
 
-const ADD_RECORD = {
-    type:
-    payload:
-}
+const addUser = () => ({ type: 'ADD_USER', payload });
+
+const addRecord = () => ({ type: 'ADD_RECORD', payload });
+
+const delRecord = () => ({ type: 'DEL_RECORD', payload });
+
+const editRecord = () = ({ type: 'EDIT_RECORD', payload });
