7064e66df322460581471dc6ac354b803de56bcc
jlfagundes <jlfagundes@gmail.com>
2021-06-10 18:02:26 -0300

    feat: (req1) insert button done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addUser } from '../actions';
 
-
-
 class Login extends React.Component {
   constructor() {
     super();
@@ -20,15 +18,23 @@ class Login extends React.Component {
   }
 
   render() {
+    const { email } = this.state;
+    console.log(email);
     return (
       <div>
         login
         <form>
           <label htmlFor="e-mail">
             E-mail:
-            <input type="text" data-testid="email-input" name="email" onChange={ this.changleChange } />
+            <input
+              type="text"
+              data-testid="email-input"
+              name="email"
+              onChange={ this.changleChange }
+            />
             <input type="text" data-testid="password-input" name="email" />
           </label>
+          <button type="submit">Entrar</button>
         </form>
       </div>
     );
