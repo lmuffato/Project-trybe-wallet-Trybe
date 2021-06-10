6ad7b04665243d8ebf2ef2bb173a586d76b035e1
mallmann02 <leomallmann020302@gmail.com>
2021-06-10 18:21:24 -0300

    feat: Actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,38 @@
-export const login = (payload) => ({type: 'LOGIN', payload})
+import { getCurrentExchangeRates } from '../services/api';
+
+export const login = (payload) => ({ type: 'LOGIN', payload });
+
+export const fetchCurrentExchangeRates = () => ({
+  type: 'FETCH_CURRENT_EXCHANGE_RATES',
+});
+
+export const getCurrentExchangeRatesError = (error) => ({
+  type: 'GET_CURRENT_EXCHANGE_ERROR',
+  error,
+});
+
+export const addExpense = (payload) => ({
+  type: 'ADD_EXPENSE',
+  payload,
+});
+
+export const deleteExpense = (payload) => ({
+  type: 'DELETE_EXPENSE',
+  payload,
+});
+
+export const addExpensesThunk = (expense) => (dispatch) => {
+  dispatch(fetchCurrentExchangeRates());
+
+  getCurrentExchangeRates()
+    .then((res) => {
+      const exchangeRates = res;
+      dispatch(addExpense({
+        ...expense,
+        exchangeRates,
+      }));
+    })
+    .catch((error) => {
+      dispatch(getCurrentExchangeRatesError(error));
+    });
+};
