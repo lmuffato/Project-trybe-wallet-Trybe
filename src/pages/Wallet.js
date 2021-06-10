11a7413dddfcd8919e78aaaadf50f23dc4d66e3e
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-06-10 16:25:59 -0300

    Req5 - Just header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{`Email: ${email}`}</span>
+        <span data-testid="total-field">{`Valor: ${0}`}</span>
+        <span data-testid="header-currency-field">{`Moeda: ${'BRL'}`}</span>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
