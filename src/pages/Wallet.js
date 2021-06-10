2179e45b25881637a102771be5751b998a9a37ba
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-06-10 17:52:31 -0300

    Req6: Forms - done it

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,11 +6,47 @@ class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <span data-testid="email-field">{`Email: ${email}`}</span>
-        <span data-testid="total-field">{`Valor: ${0}`}</span>
-        <span data-testid="header-currency-field">{`Moeda: ${'BRL'}`}</span>
-      </header>
+      <div>
+        <header>
+          <span data-testid="email-field">{`Email: ${email}`}</span>
+          <span data-testid="total-field">{`Despesa Total: ${0}`}</span>
+          <span data-testid="header-currency-field">{`Moeda: ${'BRL'}`}</span>
+        </header>
+        <form data-testid="wallet-form">
+          <label htmlFor="number-in" data-testid="number-input-label">
+            Valor
+            <input type="number" id="number-in" />
+          </label>
+          <label htmlFor="text-in" data-testid="text-input-label">
+            Descrição
+            <input type="text" id="text-in" />
+          </label>
+          <label htmlFor="selected-in" data-testid="select-input-label">
+            Moeda
+            <select id="selected-in">
+              <option> </option>
+            </select>
+          </label>
+          <label htmlFor="selected-in-payment" data-testid="select-label-payment">
+            Método de pagamento
+            <select id="selected-in-payment">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="selected-in-tag" data-testid="select-label-tag">
+            Tag
+            <select id="selected-in-tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
