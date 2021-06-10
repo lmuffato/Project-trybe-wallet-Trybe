5be555be79cc752584661fbebd6120236933ec04
mallmann02 <leomallmann020302@gmail.com>
2021-06-10 18:20:48 -0300

    Import css file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
+import './App.css';
+
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login }/>
-      <Route path="/carteira" component={ Wallet }/>
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
     </Switch>
   );
 }
