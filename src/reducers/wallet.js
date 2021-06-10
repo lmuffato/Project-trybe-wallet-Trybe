257af1aa60bf83f8b6455f00f3dd557b19b88f66
mallmann02 <leomallmann020302@gmail.com>
2021-06-10 18:24:51 -0300

    feat: Wallet reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,26 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  expenses: [],
+  totalSpent: 0,
+  currentId: 0,
+};
+
+function walletReducer(state = initialState, action) {
+  switch (action.type) {
+  case 'ADD_EXPENSE':
+    console.log(state.totalSpent);
+    return {
+      expenses: [...state.expenses, action.payload],
+      currentId: state.currentId + 1,
+    };
+  case 'DELETE_EXPENSE':
+    console.log(state.totalSpent);
+    return {
+      ...state,
+      expenses: state.expenses.filter(({ id }) => id !== action.payload.id),
+    };
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
