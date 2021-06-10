88aa23dbabd0dfd3a70cef4026cc2a62b229189f
Willian Mendes <willianmendesbr@gmail.com>
2021-06-10 13:08:30 -0300

    feat: add validation to email and password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <form>
-        <div>
-          <input
-            type="email"
-            placeholder="E-mail"
-            aria-label="email"
-            data-testid="email-input"
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            placeholder="Senha"
-            aria-label="senha"
-            data-testid="password-input"
-          />
-        </div>
-        <button type="submit">Entrar</button>
-      </form>
-    );
-  }
-}
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+
+  };
+
+  const validateEmail = (emailToVal) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(emailToVal);
+  };
+
+  const validatePassword = (passwordToVal) => passwordToVal.length >= 6;
+
+  const validate = () => {
+    const emailIsValid = validateEmail(email);
+    const passwordIsValid = validatePassword(password);
+    return emailIsValid && passwordIsValid;
+  };
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <div>
+        <input
+          name="email"
+          type="email"
+          placeholder="E-mail"
+          onChange={ (event) => setEmail(event.target.value) }
+          aria-label="email"
+          data-testid="email-input"
+        />
+      </div>
+      <div>
+        <input
+          name="password"
+          type="password"
+          placeholder="Senha"
+          onChange={ (event) => setPassword(event.target.value) }
+          aria-label="senha"
+          data-testid="password-input"
+        />
+      </div>
+      <button type="submit" disabled={ !validate() }>Entrar</button>
+    </form>
+  );
+};
 
 export default Login;
