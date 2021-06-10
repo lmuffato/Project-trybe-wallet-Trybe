4cbba4536f1803e76f37d05a90e884c329cc7f3a
rcalvs <rafaelcalvette@gmail.com>
2021-06-10 16:07:56 -0300

    Req 4 pt 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_EXPENSE = 'SAVE_EXPENSE';
 
 export function saveEmail(email) {
   return { type: SAVE_EMAIL, email };
+}
+
+export function saveExpense(expenseDetails) {
+  return { type: SAVE_EXPENSE, expenseDetails };
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import { string } from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sumConvertedValues = this.sumConvertedValues.bind(this);
+  }
+
+  sumConvertedValues() {
+    const { expenses } = this.props;
+    let total = 0;
+    expenses.forEach((expense) => {
+      total += expense.value * expense.exchangeRates[expense.currency].ask;
+    });
+    return parseFloat(total.toFixed(2));
+  }
+
   render() {
     const { email } = this.props;
     return (
       <header>
-        <p data-testid="email-field">{ email }</p>
-        <p data-testid="total-field">0</p>
+        <p data-testid="email-field">
+          E-mail:
+          { email }
+        </p>
+        <p data-testid="total-field">{ this.sumConvertedValues() }</p>
         <p data-testid="header-currency-field">BRL</p>
       </header>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
 Header.propTypes = {
   email: string,
 }.isRequired;
 
-export default Header;
+export default connect(mapStateToProps)(Header);
