131055ae69809360a8bcdebc8017d6dd43949b42
vinicios <soicinivt@gmail.com>
2021-06-10 17:39:37 -0300

    Login page validation.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../Login.css';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.isEnebledTheButton = this.isEnebledTheButton.bind(this);
+    this.createEmailAndPasswordState = this.createEmailAndPasswordState.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  isEnebledTheButton() {
+    const { email, password } = this.state;
+    const Valid = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    const isValidEmail = Valid.test(email);
+    const minLength = 6;
+    const passwordLength = password.length >= minLength;
+    console.log(isValidEmail, passwordLength);
+    return (isValidEmail && passwordLength);
+  }
+
+  async createEmailAndPasswordState({ target: { value, type } }) {
+    console.log(value, type);
+    await this.setState({ [type]: value });
+    return this.isEnebledTheButton();
+  }
+
   render() {
     return (
       <div className="login">
         <label htmlFor="emailInput">
           Login:
           <input
+            onChange={ this.createEmailAndPasswordState }
             className="inputBox"
             id="emailInput"
-            type="password"
+            type="email"
             data-testid="email-input"
             required
           />
@@ -18,15 +46,17 @@ class Login extends React.Component {
         <label htmlFor="passwordInput">
           Password:
           <input
+            onChange={ this.createEmailAndPasswordState }
             className="inputBox"
             id="passwordInput"
             type="password"
             data-testid="password-input"
             required
-            minLength="8"
           />
         </label>
-        <button type="button">Entrar</button>
+        <button disabled={ !this.isEnebledTheButton() } type="button">
+          Entrar
+        </button>
       </div>
     );
   }
