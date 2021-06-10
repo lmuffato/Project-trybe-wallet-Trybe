1a22200938e243665593f755289e0b292cbc9d15
guidugaich <guidugaich.dev@gmail.com>
2021-06-10 18:27:42 -0300

    email and password validation functions and conditional disable of button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,66 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      hasValidLogin: false
+    }
+  }
+
+  handleChange = ({target: { name, value }}) => {
+    this.setState({[name]: value }, () => {
+      if (this.hasValidEmail() && this.hasValidPassword()) {
+        this.setState({hasValidLogin: true})
+      } else {
+        this.setState({hasValidLogin: false})
+      }
+    })
+  }
+
+  hasValidEmail = () => {
+    const { email } = this.state;
+    const emailRegex = /[a-z0-9]+@[a-z0-9]+(\.com)$/gi
+    return emailRegex.test(email)
+  }
+
+  hasValidPassword = () => {
+    const { password } = this.state;
+    return password.length >= 6;
+  }
+
   render() {
+    const { hasValidLogin } = this.state;
+
     return (
       <div>
         <form>
           <label htmlFor="email">
             Email:
             <input
-              type="text"
+              type="email"
               name="email"
               id="email"
               data-testid="email-input"
+              onChange={ this.handleChange }
             />
           </label>
           <label htmlFor="password">
             Password:
             <input
               type="password"
-              name="email"
+              name="password"
               id="password"
               data-testid="password-input"
+              onChange={ this.handleChange }
             />
           </label>
-          <button type="submit">
+          <button
+            type="submit"
+            disabled={!hasValidLogin} 
+          >
             Entrar
           </button>
         </form>
