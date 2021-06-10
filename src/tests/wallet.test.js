d68fa0476da75824178fedec0250acdcc71f5fc3
igson <igson660@gmail.com>
2021-06-09 22:23:57 -0500

    quesito 04 compelto

--- src/tests/wallet.test.js
+++ src/tests/wallet.test.js
@@ -17,14 +17,14 @@ const mockedExchange = jest.spyOn(global, 'fetch').mockImplementation(() => apiR
 afterEach(() => jest.clearAllMocks());
 
 describe('4 - Crie uma página para sua carteira com as seguintes características:', () => {
-  test.skip('A rota para esta página deve ser \'/carteira\'', () => {
+  test('A rota para esta página deve ser \'/carteira\'', () => {
     const { history } = renderWithRouterAndStore(<App />);
     history.push('/carteira');
     const email = screen.queryByTestId('email-input');
     expect(email).toBeNull();
   });
 
-  test.skip('O componente deve se chamar Wallet e estar localizado na pasta "src/pages"', () => {
+  test('O componente deve se chamar Wallet e estar localizado na pasta "src/pages"', () => {
     const { container } = renderWithRouterAndStore(<Wallet />, '/carteira', {});
     expect(container).toBeDefined();
   });
