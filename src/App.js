02daa50ccaa0a824f5ce7ee10bb154b2aab2d71a
jlfagundes <jlfagundes@gmail.com>
2021-06-10 15:58:35 -0300

    fix: (config-reducer) corrections initial state reducers user and wallet

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Hello, TrybeWallet!
         <Switch>
           <Route path="/" component={ Login } />
         </Switch>
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,9 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 const INITIAL_STATE = {
-  user: {
-    email: '',
-  },
+  email: '',
 };
 
-const userReducer = (state = INITIAL_STATE, action) => {
+const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case 'NEW_ACTION':
     return state;
@@ -14,4 +12,4 @@ const userReducer = (state = INITIAL_STATE, action) => {
   }
 };
 
-export default userReducer;
+export default user;
