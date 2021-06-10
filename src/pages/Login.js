b1a0c1dff1767a56e492c370bd9f95b37522cec3
Willian Mendes <willianmendesbr@gmail.com>
2021-06-10 12:27:13 -0300

    feat: basic structure for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <div>
+          <input
+            type="email"
+            placeholder="E-mail"
+            aria-label="email"
+            data-testid="email-input"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Senha"
+            aria-label="senha"
+            data-testid="password-input"
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
