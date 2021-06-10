b89b3a889fd8abd55d7a262feeb74ac8a07a4216
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-06-10 00:15:14 -0300

    Req2: done it

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,21 +6,59 @@ class Login extends React.Component {
     this.state = {
       email: '',
       senha: '',
+      disableButton: true,
+      validateEmail: false,
+      validateSenha: false,
     };
     this.handleEmail = this.handleEmail.bind(this);
     this.handleSenha = this.handleSenha.bind(this);
+    this.enableBtn = this.enableBtn.bind(this);
   }
+
+  enableBtn() {
+    const { validateEmail, validateSenha } = this.state;
+    if (validateEmail && validateSenha) {
+      this.setState({ disableButton: false });
+    } else {
+      this.setState({ disableButton: true });
+    }
+  }
+
+  // https://www.w3resource.com/javascript/form/email-validation.php  - para validar email
 
   handleEmail(event) {
     this.setState({ email: event.target.value });
+
+    const { email } = this.state;
+    const validate = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)/;
+    if (validate.test(email)) {
+      this.setState({
+        validateEmail: true,
+      }, () => { this.enableBtn(); });
+    } else {
+      this.setState({
+        validateEmail: false,
+      }, () => { this.enableBtn(); });
+    }
   }
 
   handleSenha(event) {
     this.setState({ senha: event.target.value });
+    const { senha } = this.state;
+    const noMagicNum = 5;
+    if (senha.length >= noMagicNum) {
+      this.setState({
+        validateSenha: true,
+      }, () => { this.enableBtn(); });
+    } else {
+      this.setState({
+        validateSenha: false,
+      }, () => { this.enableBtn(); });
+    }
   }
 
   render() {
-    const { email, senha } = this.state;
+    const { email, senha, disableButton } = this.state;
     return (
       <div>
         <div>
@@ -41,6 +79,7 @@ class Login extends React.Component {
         </div>
         <button
           type="button"
+          disabled={ disableButton }
         >
           Entrar
         </button>
