fdbeb731782cd37908c0d93f475eb8a0a2726e8d
mallmann02 <leomallmann020302@gmail.com>
2021-06-10 18:24:12 -0300

    feat: Customer's wallet page

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,29 +1,122 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { addExpensesThunk as addExpenseAction,
+  deleteExpense as deleteExpenseAction } from '../actions';
+
+import WalletForm from '../components/WalletForm';
+import WalletTable from '../components/WalletTable';
 
 class Wallet extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: 0,
+      description: '',
+      tag: '',
+      method: '',
+      currency: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.calcTotalSpent = this.calcTotalSpent.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { wallet: { currentId } } = this.props;
+    const { addExpense } = this.props;
+    addExpense({
+      id: currentId,
+      ...this.state,
+    });
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  handleDelete(expense) {
+    const { deleteExpense } = this.props;
+    deleteExpense(expense);
+  }
+
+  calcTotalSpent() {
+    const { wallet: { expenses } } = this.props;
+    const total = expenses.reduce(
+      (acc, cur) => parseFloat(cur.value * cur.exchangeRates[cur.currency].ask) + acc, 0,
+    );
+    return total;
+  }
+
   render() {
-    const {userEmail} = this.props;
+    const { userEmail, wallet: { expenses } } = this.props;
+    const {
+      value,
+      description,
+      tag,
+      method,
+      currency,
+    } = this.state;
+    const walletFormProps = {
+      onSubmit: this.handleSubmit,
+      onChange: this.handleChange,
+      value,
+      description,
+      tag,
+      method,
+      currency,
+    };
+
     return (
-      <div>
+      <div className="wallet">
         <header>
-          <h3 data-testid="email-field">
+          <p data-testid="email-field">
             {userEmail.email}
-          </h3>
-          <span data-testid="total-field">
-            0
-          </span>
+          </p>
+          <p data-testid="total-field">
+            {this.calcTotalSpent()}
+          </p>
           <legend data-testid="header-currency-field">
             BRL
           </legend>
         </header>
+        <WalletForm
+          { ...walletFormProps }
+        />
+        <WalletTable
+          expenses={ expenses }
+          onDelete={ this.handleDelete }
+        />
       </div>
     );
   }
+}
+
+Wallet.propTypes = {
+  addExpense: PropTypes.func.isRequired,
+  deleteExpense: PropTypes.func.isRequired,
+  wallet: PropTypes.shape({
+    expenses: PropTypes.objectOf(PropTypes.string),
+    totalSpent: PropTypes.number,
+    currentId: PropTypes.number,
+  }).isRequired,
+  userEmail: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 const mapStateToProps = (state) => ({
   userEmail: state.user,
+  wallet: state.wallet,
 });
 
-export default connect(mapStateToProps, null)(Wallet);
+const mapDispatchToProps = (dispatch) => ({
+  addExpense: (e) => dispatch(addExpenseAction(e)),
+  deleteExpense: (e) => dispatch(deleteExpenseAction(e)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
