3eb460dbd4a16aa449a5e923ac466410215f0a2d
Willian Mendes <willianmendesbr@gmail.com>
2021-06-10 12:25:49 -0300

    feat: initial reducer config

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  state: '',
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'Test':
+    return { state: action.state };
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  state: '',
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'Test':
+    return { state: action.state };
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
