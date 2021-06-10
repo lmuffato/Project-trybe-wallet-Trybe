1ff0e305134a9232d75fbf4bf25dca14b869b49b
Patrick Morais <ppternunes@gmail.com>
2021-06-10 05:23:50 -0300

    requisito 8 ask undefined

--- src/components/getAsk.js
+++ src/components/getAsk.js
@@ -2,7 +2,8 @@ const getAsk = async (curr) => {
   const endpoint = `https://economia.awesomeapi.com.br/json/${curr}-BRL`;
   const response = await fetch(endpoint);
   const json = await response.json();
-  const algo = json[0].ask;
+  const arr = json[0];
+  const algo = arr.ask;
   const inteiro = +(parseFloat(algo).toFixed(2));
   return inteiro;
 };
