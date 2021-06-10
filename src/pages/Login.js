d53b3a993894685665bc9b5b860b95208c9fc8c0
defauth98 <neto.daniribeiro@gmail.com>
2021-06-10 11:38:26 -0300

    req 2: validacao do email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,8 +3,86 @@ import React from 'react';
 import '../styles/pages/login.css';
 import trybeLogo from '../assets/trybe-logo.png';
 
+const passwordMinlength = 6;
+
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+      validEmail: false,
+      validPassword: false,
+    };
+  }
+
+  // Consultei o stack overflow para ver como é feito a regex
+  // link: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+  verifyEmail(email) {
+    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+    return regex.test(email);
+  }
+
+  verifyPassword(password) {
+    if (password.length >= passwordMinlength) {
+      return true;
+    }
+
+    return false;
+  }
+
+  handleInputChange(event) {
+    const { target: { type, value } } = event;
+
+    if (type === 'email') {
+      this.setState({ email: value });
+
+      if (this.verifyEmail(value)) {
+        this.setState({ validEmail: true });
+      } else {
+        this.setState({ validEmail: false });
+      }
+    } else {
+      this.setState({ password: value });
+
+      if (this.verifyPassword(value)) {
+        this.setState({ validPassword: true });
+      } else {
+        this.setState({ validPassword: false });
+      }
+    }
+  }
+
+  renderButton(validEmail, validPassword) {
+    if (validEmail && validPassword) {
+      return (
+        <button
+          type="button"
+          className="button-active"
+        >
+          Entrar
+        </button>
+      );
+    }
+
+    return (
+      <button
+        type="button"
+        disabled
+        className="button-disable"
+      >
+        Entrar
+      </button>
+    );
+  }
+
   render() {
+    const { email, password, validEmail, validPassword } = this.state;
+
     return (
       <div className="login-page">
         <img src={ trybeLogo } alt="Logo da Trybe" />
@@ -12,6 +90,8 @@ class Login extends React.Component {
           type="email"
           data-testid="email-input"
           placeholder="seu@email.com"
+          value={ email }
+          onChange={ this.handleInputChange }
         />
         <input
           type="password"
@@ -19,8 +99,12 @@ class Login extends React.Component {
           id="password"
           data-testid="password-input"
           placeholder="*******"
+          value={ password }
+          onChange={ this.handleInputChange }
         />
-        <button type="button">Entrar</button>
+
+        {this.renderButton(validEmail, validPassword)}
+
       </div>);
   }
 }
