c21a1847fb63c2d7426b3a3055dfbe62b7fad92d
Lucas Pedroso <lepsk8@gmail.com>
2021-06-11 14:10:37 -0300

    feat: :sparkles: Add reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER_LOGIN } from '../actions';
+
+const initialState = {
+  email: '',
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+  case USER_LOGIN:
+    return { ...state, email: payload };
+
+  default:
+    return state;
+  }
+};
