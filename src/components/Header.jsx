61bef100e39c8cc1d72fa97ca3af67de1be2f223
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-06-11 08:12:16 -0300

    Finalmente Requisito 8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,30 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  totalCalc() {
+    const { expenses } = this.props;
+    const expenseValues = expenses.map((expense) => {
+      const spendCurrency = expense.currency;
+      const currencyRate = expense.exchangeRates[spendCurrency].ask;
+      const convertedValue = parseFloat(expense.value) * parseFloat(currencyRate);
+      return convertedValue;
+    });
+    const totalSum = expenseValues.reduce((acc, cur) => acc + parseFloat(cur), 0);
+    return totalSum;
+  }
+
   render() {
     const { user } = this.props;
     return (
       <header className="wallet-header">
         <h6 data-testid="email-field">{user}</h6>
         <p data-testid="header-currency-field">Câmbio: BRL</p>
-        <p data-testid="total-field">Valor total: 0</p>
+        <p data-testid="total-field">
+          Valor Total:
+          {' '}
+          {this.totalCalc()}
+          ;
+        </p>
       </header>
     );
   }
@@ -17,6 +34,7 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   user: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
