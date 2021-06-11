939bd92de97138e961b7c056e82b2a212dbfd748
Willian Mendes <willianmendesbr@gmail.com>
2021-06-11 11:45:23 -0300

    style: fix eslint

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -1,53 +1,50 @@
 import React from 'react';
 
-const AddExpense = () => {
-  const test = '1';
-  return (
-    <form>
-      <label className="form-label mb-3" htmlFor="valueExpense">
-        Valor
-        <input
-          id="valueExpense"
-          name="valueExpense"
-          className="form-control"
-          type="number"
-        />
-      </label>
-      <label className="form-label mb-3" htmlFor="descriptionExpense">
-        Descrição
-        <input
-          id="descriptionExpense"
-          name="descriptionExpense"
-          className="form-control"
-          type="text"
-        />
-      </label>
-      <label className="form-label mb-3" htmlFor="currency">
-        Moeda
-        <select id="currency" name="currency" className="form-select">
-          <option>Selecione uma moeda</option>
-        </select>
-      </label>
-      <label className="form-label mb-3" htmlFor="paymentMethod">
-        Método de pagamento
-        <select id="paymentMethod" name="paymentMethod" className="form-select">
-          <option>Dinheiro</option>
-          <option>Cartão de crédito</option>
-          <option>Cartão de débito</option>
-        </select>
-      </label>
-      <label className="form-label mb-3" htmlFor="tag">
-        Tag
-        <select id="tag" name="tag" className="form-select">
-          <option>Alimentação</option>
-          <option>Lazer</option>
-          <option>Trabalho</option>
-          <option>Transporte</option>
-          <option>Saúde</option>
-        </select>
-      </label>
-    </form>
-  );
-};
+const AddExpense = () => (
+  <form className="container">
+    <label className="form-label mb-3" htmlFor="valueExpense">
+      Valor
+      <input
+        id="valueExpense"
+        name="valueExpense"
+        className="form-control"
+        type="number"
+      />
+    </label>
+    <label className="form-label mb-3" htmlFor="descriptionExpense">
+      Descrição
+      <input
+        id="descriptionExpense"
+        name="descriptionExpense"
+        className="form-control"
+        type="text"
+      />
+    </label>
+    <label className="form-label mb-3" htmlFor="currency">
+      Moeda
+      <select id="currency" name="currency" className="form-select">
+        <option>Selecione uma moeda</option>
+      </select>
+    </label>
+    <label className="form-label mb-3" htmlFor="paymentMethod">
+      Método de pagamento
+      <select id="paymentMethod" name="paymentMethod" className="form-select">
+        <option>Dinheiro</option>
+        <option>Cartão de crédito</option>
+        <option>Cartão de débito</option>
+      </select>
+    </label>
+    <label className="form-label mb-3" htmlFor="tag">
+      Tag
+      <select id="tag" name="tag" className="form-select">
+        <option>Alimentação</option>
+        <option>Lazer</option>
+        <option>Trabalho</option>
+        <option>Transporte</option>
+        <option>Saúde</option>
+      </select>
+    </label>
+  </form>
+);
 
 export default AddExpense;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   const user = useSelector((state) => state.user);
 
   return (
-    <header className="w-100">
-      <div className="container">
-        <div>
+    <header className="w-100 container mb-3">
+      <div className="row">
+        <div className="col">
           <span>E-mail: </span>
           <span data-testid="email-field">{user.email}</span>
         </div>
-        <div>
+        <div className="col">
           <span>Despesa Total: </span>
           <span data-testid="total-field">0</span>
           <span data-testid="header-currency-field">BRL</span>
