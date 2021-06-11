6b4eb368f152dd6b9a19c3d7da0ed06914333de3
Willian Mendes <willianmendesbr@gmail.com>
2021-06-11 12:24:31 -0300

    feat: load currencies from api

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -1,50 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getCurrencies from '../service/Api';
 
-const AddExpense = () => (
-  <form className="container">
-    <label className="form-label mb-3" htmlFor="valueExpense">
-      Valor
-      <input
-        id="valueExpense"
-        name="valueExpense"
-        className="form-control"
-        type="number"
-      />
-    </label>
-    <label className="form-label mb-3" htmlFor="descriptionExpense">
-      Descrição
-      <input
-        id="descriptionExpense"
-        name="descriptionExpense"
-        className="form-control"
-        type="text"
-      />
-    </label>
-    <label className="form-label mb-3" htmlFor="currency">
-      Moeda
-      <select id="currency" name="currency" className="form-select">
-        <option>Selecione uma moeda</option>
-      </select>
-    </label>
-    <label className="form-label mb-3" htmlFor="paymentMethod">
-      Método de pagamento
-      <select id="paymentMethod" name="paymentMethod" className="form-select">
-        <option>Dinheiro</option>
-        <option>Cartão de crédito</option>
-        <option>Cartão de débito</option>
-      </select>
-    </label>
-    <label className="form-label mb-3" htmlFor="tag">
-      Tag
-      <select id="tag" name="tag" className="form-select">
-        <option>Alimentação</option>
-        <option>Lazer</option>
-        <option>Trabalho</option>
-        <option>Transporte</option>
-        <option>Saúde</option>
-      </select>
-    </label>
-  </form>
-);
+const AddExpense = () => {
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    const fetchCurrencies = async () => {
+      const currenciesData = await getCurrencies();
+      setCurrencies(currenciesData);
+    };
+
+    fetchCurrencies().then();
+  }, []);
+
+  return (
+    <form className="container">
+      <label className="form-label mb-3" htmlFor="valueExpense">
+        Valor
+        <input
+          id="valueExpense"
+          name="valueExpense"
+          className="form-control"
+          type="number"
+        />
+      </label>
+      <label className="form-label mb-3" htmlFor="descriptionExpense">
+        Descrição
+        <input
+          id="descriptionExpense"
+          name="descriptionExpense"
+          className="form-control"
+          type="text"
+        />
+      </label>
+      <label className="form-label mb-3" htmlFor="currency">
+        Moeda
+        <select id="currency" name="currency" className="form-select">
+          {
+            currencies.map((currency, index) => (
+              <option key={ index }>
+                {currency.code}
+              </option>
+            ))
+          }
+        </select>
+      </label>
+      <label className="form-label mb-3" htmlFor="paymentMethod">
+        Método de pagamento
+        <select id="paymentMethod" name="paymentMethod" className="form-select">
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+        </select>
+      </label>
+      <label className="form-label mb-3" htmlFor="tag">
+        Tag
+        <select id="tag" name="tag" className="form-select">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </select>
+      </label>
+    </form>
+  );
+};
 
 export default AddExpense;
---
+++ src/service/Api.js
@@ -0,0 +1,11 @@
+const CURRENCY_URL = 'https://economia.awesomeapi.com.br/json/all';
+
+const getCurrencies = async () => {
+  const currenciesApi = await fetch(CURRENCY_URL);
+  const currenciesJson = await currenciesApi.json();
+  const currencies = Object
+    .values(currenciesJson).filter((currency) => currency.codein === 'BRL');
+  return currencies;
+};
+
+export default getCurrencies;
