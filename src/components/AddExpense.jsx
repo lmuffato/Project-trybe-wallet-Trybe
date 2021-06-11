7ef67c6aadfca1a0aad38f0bced8d93f63e5ca69
Willian Mendes <willianmendesbr@gmail.com>
2021-06-11 12:29:15 -0300

    style: fix eslint

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -1,6 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import getCurrencies from '../service/Api';
 
+const template = (currencies) => (
+  <form className="container">
+    <label className="form-label mb-3" htmlFor="valueExpense">
+      Valor
+      <input id="valueExpense" className="form-control" type="number" />
+    </label>
+    <label className="form-label mb-3" htmlFor="descriptionExpense">
+      Descrição
+      <input id="descriptionExpense" className="form-control" type="text" />
+    </label>
+    <label className="form-label mb-3" htmlFor="currency">
+      Moeda
+      <select id="currency" name="currency" className="form-select">
+        {
+          currencies.map((currency, index) => (
+            <option key={ index }>
+              {currency.code}
+            </option>
+          ))
+        }
+      </select>
+    </label>
+    <label className="form-label mb-3" htmlFor="paymentMethod">
+      Método de pagamento
+      <select id="paymentMethod" name="paymentMethod" className="form-select">
+        <option>Dinheiro</option>
+        <option>Cartão de crédito</option>
+        <option>Cartão de débito</option>
+      </select>
+    </label>
+    <label className="form-label mb-3" htmlFor="tag">
+      Tag
+      <select id="tag" name="tag" className="form-select">
+        <option>Alimentação</option>
+        <option>Lazer</option>
+        <option>Trabalho</option>
+        <option>Transporte</option>
+        <option>Saúde</option>
+      </select>
+    </label>
+  </form>
+);
+
 const AddExpense = () => {
   const [currencies, setCurrencies] = useState([]);
 
@@ -13,58 +56,7 @@ const AddExpense = () => {
     fetchCurrencies().then();
   }, []);
 
-  return (
-    <form className="container">
-      <label className="form-label mb-3" htmlFor="valueExpense">
-        Valor
-        <input
-          id="valueExpense"
-          name="valueExpense"
-          className="form-control"
-          type="number"
-        />
-      </label>
-      <label className="form-label mb-3" htmlFor="descriptionExpense">
-        Descrição
-        <input
-          id="descriptionExpense"
-          name="descriptionExpense"
-          className="form-control"
-          type="text"
-        />
-      </label>
-      <label className="form-label mb-3" htmlFor="currency">
-        Moeda
-        <select id="currency" name="currency" className="form-select">
-          {
-            currencies.map((currency, index) => (
-              <option key={ index }>
-                {currency.code}
-              </option>
-            ))
-          }
-        </select>
-      </label>
-      <label className="form-label mb-3" htmlFor="paymentMethod">
-        Método de pagamento
-        <select id="paymentMethod" name="paymentMethod" className="form-select">
-          <option>Dinheiro</option>
-          <option>Cartão de crédito</option>
-          <option>Cartão de débito</option>
-        </select>
-      </label>
-      <label className="form-label mb-3" htmlFor="tag">
-        Tag
-        <select id="tag" name="tag" className="form-select">
-          <option>Alimentação</option>
-          <option>Lazer</option>
-          <option>Trabalho</option>
-          <option>Transporte</option>
-          <option>Saúde</option>
-        </select>
-      </label>
-    </form>
-  );
+  return template(currencies);
 };
 
 export default AddExpense;
