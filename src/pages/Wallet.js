b3063c7894deeb30ba452b2eaa4cc1718a613653
Douglas <douguinhodns@gmail.com>
2021-06-11 14:46:55 -0300

    req 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -7,14 +7,50 @@ class Wallet extends React.Component {
     const { emailGot } = this.props;
     console.log(emailGot);
     return (
-      <header>
-        <span data-testid="email-field">
-          Olá,
-          { emailGot }
-        </span>
-        <span data-testid="total-field">Despesa Total: R$ 0 </span>
-        <span data-testid="header-currency-field">BRL</span>
-      </header>
+      <>
+        <header>
+          <span data-testid="email-field">
+            Olá,
+            { emailGot }
+          </span>
+          <span data-testid="total-field">Despesa Total: R$ 0 </span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <main>
+          <label htmlFor="input-valor">
+            Valor
+            <input type="number" id="input-valor" />
+          </label>
+          <label htmlFor="input-descrição">
+            Descrição
+            <input type="text" id="input-descrição" />
+          </label>
+          <label htmlFor="input-moeda">
+            Moeda
+            <select id="input-moeda">
+              <option>1</option>
+            </select>
+          </label>
+          <label htmlFor="input-pagamento">
+            Método de pagamento
+            <select id="input-pagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="input-categoria">
+            Tag
+            <select id="input-categoria">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </main>
+      </>
     );
   }
 }
