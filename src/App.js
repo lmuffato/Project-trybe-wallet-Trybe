bfc4970e6bdd7de4e5d15b17ed846205d9a2c4c1
Lucas Pedroso <lepsk8@gmail.com>
2021-06-11 14:08:39 -0300

    feat: :sparkles: Add Routes [/, /carteira, *]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
+  );
 }
 
 export default App;
