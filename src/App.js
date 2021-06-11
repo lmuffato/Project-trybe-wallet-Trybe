c39c4b3051d7760c1fb4ed6da42a80ce42ca25e1
Mateus Alencar <mtauws@gmail.com>
2021-06-10 22:25:54 -0300

    chore: req1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import Login from './pages/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
+import logo from '../assets/trybe.png';
+import './Login.css';
+
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form className="flex login">
+        <fieldset>
+          <img src={ logo } alt="Logo da trybe." />
+          <label htmlFor="email">
+            Email
+            <input
+              id="email"
+              className="input"
+              data-testid="email-input"
+              type="email"
+              placeholder="Email"
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              id="password"
+              className="input"
+              data-testid="password-input"
+              type="password"
+              placeholder="Password"
+            />
+          </label>
+
+          <button type="submit">Entrar</button>
+        </fieldset>
+      </form>
+    );
   }
 }
 
