2e920548975ab94376c6236701774acfef7e77a0
LucasMLara <lucasml@live.com>
2021-06-10 21:44:21 -0300

    Req 5 Implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import store from '../store/index';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
 class Header extends Component {
   render() {
-    console.log(store.getState());
-    const { user: { email } } = store.getState();
+    const { displayEmail } = this.props;
+    console.log(displayEmail);
     return (
       <div>
-        <header data-testid="email-field">{ email }</header>
+        <header data-testid="email-field">{ displayEmail }</header>
         <h5 data-testid="total-field">0</h5>
         <h5 data-testid="header-currency-field">BRL</h5>
       </div>);
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  displayEmail: state.user.email,
+});
+
+Header.propTypes = {
+  displayEmail: propTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
