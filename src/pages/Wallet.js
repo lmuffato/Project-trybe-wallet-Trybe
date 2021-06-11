4e2906b6a6cafb28a50d15199774791a10f7ca14
Willian Mendes <willianmendesbr@gmail.com>
2021-06-11 11:40:58 -0300

    feat: form add expense

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import AddExpense from '../components/AddExpense';
 
 const Wallet = () => (
   <div>
     <Header />
+    <AddExpense />
   </div>
 );
 
