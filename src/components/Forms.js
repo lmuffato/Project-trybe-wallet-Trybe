12d44751b40826519490c69602e06ae7fcc623af
Marcelo Maurício <mmauricio.design@gmail.com>
2021-06-12 10:38:36 -0300

    feat: create a state on component to catch input values - requirement 8

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -8,9 +8,17 @@ class Forms extends React.Component {
     super(props);
 
     this.allCurrencies = this.allCurrencies.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 
     this.state = {
       currenciesState: [],
+      id: 0,
+      value: 0,
+      description: '',
+      currency: 'USD',
+      payment: 'Dinheiro',
+      tag: 'Alimentação',
+
     };
   }
 
@@ -34,7 +42,7 @@ class Forms extends React.Component {
     return (
       <label htmlFor="currencies">
         Moeda:
-        <select id="currencies">
+        <select id="currency" onChange={ this.handleChange }>
           { filteredCurrency.map((currency) => (
             <option
               key={ currency.code }
@@ -47,41 +55,51 @@ class Forms extends React.Component {
     );
   }
 
+  handleChange(event) {
+    const { id, value } = event.target;
+    this.setState({
+      [id]: value,
+    });
+  }
+
   render() {
     return (
-      <form>
-        <label htmlFor="value">
-          Valor:
-          <input type="number" id="value" />
-        </label>
+      <>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input type="number" id="value" onChange={ this.handleChange } />
+          </label>
 
-        <label htmlFor="description">
-          Descrição:
-          <input type="text" id="description" />
-        </label>
+          <label htmlFor="description">
+            Descrição:
+            <input type="text" id="description" onChange={ this.handleChange } />
+          </label>
 
-        {this.currencies()}
+          {this.currencies()}
 
-        <label htmlFor="payment">
-          Método de pagamento
-          <select id="payment">
-            <option value="Dinheiro">Dinheiro</option>
-            <option value="Cartão de crédito">Cartão de crédito</option>
-            <option value="Cartão de débito">Cartão de débito</option>
-          </select>
-        </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select id="payment" onChange={ this.handleChange }>
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
 
-        <label htmlFor="tag">
-          Tag:
-          <select id="tag">
-            <option value="Alimentação">Alimentação</option>
-            <option value="Lazer">Lazer</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Transporte">Transporte</option>
-            <option value="Saúde">Saúde</option>
-          </select>
-        </label>
-      </form>
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag" onChange={ this.handleChange }>
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
+        <button type="submit">Adicionar despesa</button>
+      </>
     );
   }
 }
