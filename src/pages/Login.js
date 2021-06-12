e27eb3efede3817968fab92b82d2eb4de8c1e35d
guilhermemd <guilherme.md@gmail.com>
2021-06-12 00:19:25 -0300

    feat: :rocket: req 2 - done
    implementation of req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,79 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { func } from 'prop-types';
+import { user as userlogin } from '../actions/index';
+// Referencias button: https://www.youtube.com/watch?v=sN9QvQAkyug
+const five = 5;
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validInput = this.validInput.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  validInput() {
+    const { password, email } = this.state;
+    const re = /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/gi;
+    if (re.test(email) && password.length > five) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
+    const { email, password } = this.state;
+    const { user } = this.props;
     return (
       <form>
-        <input type="email" data-testid="email-input" value="" placeholder="E-mail" />
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          placeholder="E-mail"
+          onChange={ this.handleChange }
+          name="email"
+        />
         <input
           type="password"
           data-testid="password-input"
-          value=""
+          value={ password }
           placeholder="Password"
+          onChange={ this.handleChange }
+          name="password"
         />
-        <button type="button">Entrar</button>
+        <Link
+          to="/carteira"
+          onClick={ () => user({ email, password }) }
+          disabled={ this.validInput() }
+        >
+          <button type="button" disabled={ this.validInput() }>Entrar</button>
+        </Link>
       </form>
     );
   }
 }
 
-export default Login;
+Login.propTypes = ({
+  user: func,
+}).isRequired;
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  userInfo: (payload) => dispatch(userlogin(payload)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
