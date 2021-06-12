ef5aa4e66e3ea7fe3de1d6905016082b728b2728
vinicios <soicinivt@gmail.com>
2021-06-12 12:12:46 -0300

    Forms in the component Wallet.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import '../Wallet.css';
 
 class Wallet extends Component {
   render() {
     const { props: { emailDaPessoa } } = this;
     return (
-      <div>
-        <header data-testid="email-field">{emailDaPessoa}</header>
-        <span data-testid="total-field">{0}</span>
-        <span data-testid="header-currency-field">BRL</span>
-      </div>
+      <>
+        <div className="flex-container">
+          <header data-testid="email-field">
+            {emailDaPessoa}
+          </header>
+          <span data-testid="header-currency-field">BRL</span>
+          <span data-testid="total-field">{0}</span>
+        </div>
+        <form className="fieldset">
+          <label htmlFor="valor">
+            Valor:
+            <input id="valor" type="text" name="Valor" />
+          </label>
+          <label htmlFor="descrição">
+            Descrição:
+            <input id="descrição" type="text" name="Descrição" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda:
+            <select id="moeda" name="Moeda">
+              <option value="valor1">Valor 1</option>
+              <option value="valor2" selected>Valor 2</option>
+              <option value="valor3">Valor 3</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag" name="Tag">
+              <option value="alimentação" selected>Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saúde">Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="metodoDePagamento">
+            Método de pagamento:
+            <select id="metodoDePagamento" name="Método de pagamento">
+              <option value="dinheiro" selected>Dinheiro</option>
+              <option value="cartão-de-crédito">Cartão de crédito</option>
+              <option value="Cartão-de-débito">Cartão de débito</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
