d3a1eec678cfac7a20eba97a567234c3dcf44052
William <w.willers@outlook.com>
2021-06-12 16:11:29 -0400

    Teste requisito 1 - Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            placeholder="Insira seu email"
+          />
+        </label>
+
+        <label htmlFor="password-input">
+          Senha
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            placeholder="Senha"
+          />
+        </label>
+
+        <button type="button">
+          Entrar
+        </button>
+      </section>
+    );
   }
 }
 
