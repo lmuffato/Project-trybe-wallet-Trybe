d8e5cbcf909553aa8238f77aac3d3031b3777aa0
Erick-Marinho <erickgaves@gmail.com>
2021-06-11 21:11:11 -0300

    filling in the select currency field

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 import updateAction from '../../actions/update.action';
 import { END_POINT } from '../../common/def';
 import getCurrency from '../../services/api';
@@ -13,27 +14,28 @@ export default function ExpenseForm() {
     }
     updateKeys();
   }, []);
-
+  const { currencies } = useSelector((state) => state.wallet);
   return (
     <form>
       <label htmlFor="valor">
-        Valor
-        <input data-testid="value-input" type="text" id="valor" />
+        Valor:
+        <input type="text" id="valor" />
       </label>
       <label htmlFor="descricao">
-        Descrição
-        <input data-testid="description-input" type="text" id="descricao" />
+        Descrição:
+        <input type="text" id="descricao" />
       </label>
       <label htmlFor="moeda">
         Moeda
-        <select data-testid="currency-input" id="moeda">
-          {/* <option value="USD" data-testid="USD">USD</option>
-          <option value="XRP" data-testid="XRP">XRP</option> */}
+        <select id="moeda">
+          { currencies.map((moeda) => (
+            <option key={ moeda } value={ moeda }>{moeda}</option>
+          ))}
         </select>
       </label>
       <label htmlFor="pagamento">
         Método de pagamento
-        <select data-testid="method-input" id="pagamento">
+        <select id="pagamento">
           <option value="Dinheiro">Dinheiro</option>
           <option value="Cartão de crédito">Cartão de crédito</option>
           <option value="Cartão de débito">Cartão de débito</option>
@@ -41,7 +43,7 @@ export default function ExpenseForm() {
       </label>
       <label htmlFor="tag">
         Tag
-        <select data-testid="method-input" id="tag">
+        <select id="tag">
           <option value="Alimentação">Alimentação</option>
           <option value="Lazer">Lazer</option>
           <option value="Trabalho">Trabalho</option>
