819bcfd5f0bf4c4763fe05542057e678584528d4
guidugaich <guidugaich.dev@gmail.com>
2021-06-12 10:38:57 -0300

    correcting a ton of eslint errors

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import login from '../actions';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
+import login from '../actions';
 
 class Login extends React.Component {
   constructor() {
@@ -10,79 +11,88 @@ class Login extends React.Component {
       email: '',
       password: '',
       hasValidLogin: false,
-      buttonClicked: false
-    }
+      buttonClicked: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.hasValidEmail = this.hasValidEmail.bind(this);
+    this.hasValidPassword = this.hasValidPassword.bind(this);
   }
 
-  handleChange = ({target: { name, value }}) => {
-    this.setState({[name]: value }, () => {
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => {
       if (this.hasValidEmail() && this.hasValidPassword()) {
-        this.setState({hasValidLogin: true})
+        this.setState({ hasValidLogin: true });
       } else {
-        this.setState({hasValidLogin: false})
+        this.setState({ hasValidLogin: false });
       }
-    })
+    });
   }
 
-  hasValidEmail = () => {
+  hasValidEmail() {
     const { email } = this.state;
-    const emailRegex = /[a-z0-9]+@[a-z0-9]+(\.com)$/gi
-    return emailRegex.test(email)
+    const emailRegex = /[a-z0-9]+@[a-z0-9]+(\.com)$/gi;
+    return emailRegex.test(email);
   }
 
-  hasValidPassword = () => {
+  hasValidPassword() {
     const { password } = this.state;
-    return password.length >= 6;
+    const maxPasswordLength = 6;
+    return password.length >= maxPasswordLength;
   }
 
   render() {
     const { email, password, hasValidLogin, buttonClicked } = this.state;
-    const { login } = this.props;
+    const { loginDispatcher } = this.props;
 
     return (
-      buttonClicked ? <Redirect to="/carteira" /> :
-      <div>
-        <form>
-          <label htmlFor="email">
-            Email:
-            <input
-              type="email"
-              name="email"
-              id="email"
-              data-testid="email-input"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="password">
-            Password:
-            <input
-              type="password"
-              name="password"
-              id="password"
-              data-testid="password-input"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <button
-            type="button"
-            disabled={!hasValidLogin}
-            onClick={
-              () => {
-                login(email, password);
-                this.setState({buttonClicked: true});
-              }
-            }
-          >
-            Entrar
-          </button>
-        </form>
-      </div>
+      buttonClicked ? <Redirect to="/carteira" />
+        : (
+          <div>
+            <form>
+              <label htmlFor="email">
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  data-testid="email-input"
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <label htmlFor="password">
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  data-testid="password-input"
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                type="button"
+                disabled={ !hasValidLogin }
+                onClick={ () => {
+                  loginDispatcher(email, password);
+                  this.setState({ buttonClicked: true });
+                } }
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
+        )
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  login: (email, password) => dispatch(login(email, password))
-})
+  loginDispatcher: (email, password) => dispatch(login(email, password)),
+});
+
+Login.propTypes = {
+  login: PropTypes.func,
+}.isRequired;
 
 export default connect(null, mapDispatchToProps)(Login);
