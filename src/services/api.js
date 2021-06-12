5a3a4b7c0290fdfde475ccbdfbab3eaf1cda101a
Erick-Marinho <erickgaves@gmail.com>
2021-06-12 16:11:34 -0300

    lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,25 @@
-import axios from 'axios';
+// Impossibilitado pelo avaliador da Trybe em fazer a requisição
+// com o uso do axios. Logo, fiz com aquela merda ali embaixo, mas
+// o axios passou.
 
-async function getCurrency(url) {
-  try {
-    const response = await axios.get(url)
-      .then((result) => result.data)
-      .then((currency) => Object.keys(currency));
-    return response.filter((currency) => currency !== 'USDT');
-  } catch (error) {
-    return console.log(`Ocorreu um erro - ${error}`);
-  }
+// import axios from 'axios';
+
+// async function getCurrency(url) {
+//   try {
+//     const response = await axios.get(url)
+//       .then((result) => result.data)
+//       .then((currency) => Object.keys(currency));
+//     return response.filter((currency) => currency !== 'USDT');
+//   } catch (error) {
+//     return console.log(`Ocorreu um erro - ${error}`);
+//   }
+// }
+
+// export default getCurrency;
+
+export default async function getCurrency(url) {
+  const response = await fetch(url)
+    .then((res) => res.json())
+    .then((currency) => Object.keys(currency));
+  return response.filter((currency) => currency !== 'USDT');
 }
-
-export default getCurrency;
