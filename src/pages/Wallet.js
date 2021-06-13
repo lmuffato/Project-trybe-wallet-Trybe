f973b355a783bca83cd4212dccd0019851eb5189
guidugaich <guidugaich.dev@gmail.com>
2021-06-13 14:31:18 -0300

    calling thunk dispatch on component

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import WalletHeader from '../components/WalletHeader';
 import WalletForm from '../components/WalletForm';
+import { getCurrenciesThunk } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { getCurrencies } = this.props;
+    getCurrencies();
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +20,8 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapDispatchToProps = (dispatch) => ({
+  getCurrencies: () => dispatch(getCurrenciesThunk()),
+});
+
+export default connect(null, mapDispatchToProps)(Wallet);
