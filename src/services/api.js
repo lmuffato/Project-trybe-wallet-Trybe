0b18db2db8aabdd6877cd1c27e515a789f1c23e0
guidugaich <guidugaich.dev@gmail.com>
2021-06-13 14:31:40 -0300

    api calling function

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,8 @@
-export const getAvailableCurrencies = () => {
+const API_URL = 'https://economia.awesomeapi.com.br/json/all';
 
+const getCurrenciesFromAPI = async () => {
+  const response = await fetch(API_URL);
+  return response.json();
 };
+
+export default getCurrenciesFromAPI;
