62033f9ea65a3a6a64cf783a21d63a6e59ad3ca1
lucas-chaves <lcchavesr150@gmail.com>
2021-06-13 11:48:25 -0300

    test requirement 6, form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 class Input extends Component {
   constructor(props) {
@@ -42,4 +42,4 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(Input);
+export default Input;
