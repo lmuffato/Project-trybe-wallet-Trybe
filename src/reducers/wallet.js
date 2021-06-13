3e2b01973bfeab4177a3309f35c2057358c2f79a
guidugaich <guidugaich.dev@gmail.com>
2021-06-13 14:31:29 -0300

    reducer logic

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,11 +1,32 @@
+import {
+  GET_CURRENCIES,
+  GET_CURRENCIES_SUCCESS,
+  GET_CURRENCIES_ERROR,
+} from '../actions';
+
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 const initialState = {
   currencies: [],
   expenses: [],
+  loading: false,
+  error: null,
 };
 
 const wallet = (state = initialState, action) => {
   switch (action.type) {
+  case GET_CURRENCIES:
+    return { ...state, loading: true };
+  case GET_CURRENCIES_SUCCESS:
+    return {
+      ...state,
+      loading: false,
+      currencies: action.currenciesList,
+    };
+  case GET_CURRENCIES_ERROR:
+    return {
+      ...state,
+      loading: false,
+    };
   default:
     return state;
   }
