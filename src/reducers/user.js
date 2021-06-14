8d766b92ba40f697013ed5b1d5a12c53fdd9858d
getuliogutemberg <getulio.dev@gmail.com>
2021-06-14 18:46:30 -0300

    'Primeiro Commit'

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INICIAL_STATE = {
+
+};
+
+function user(state = INICIAL_STATE, action) {
+  switch (action.type) {
+  case 'ACAO':
+    return state;
+  default:
+    return state;
+  }
+}
+
+export default user;
