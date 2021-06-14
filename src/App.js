2194fbb7693a92a3893af9fac697d3b1b7fd83d2
Robertson <robertson.nunes@gmail.com>
2021-06-14 12:38:10 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello TrybeWallet!</div>;
+  return (
+    <Switch>
+      {/* A rota para esta página deve ser ‘/’. */}
+      <Route path="/" component={ Login } />
+    </Switch>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,71 @@
 import React from 'react';
-
+// 1. Crie uma página inicial de login com os seguintes campos e características:
+// 2. Realize as seguintes verificações nos campos de email, senha e botão
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  /* Salve o email no estado da aplicação, com a chave email, assim que a pessoa usuária logar. */
+  handleInput({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit() {
+
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const passwordLength = 6;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          {/* Você deve criar um local para que a pessoa usuária insira seu email e senha.
+          */}
+          <label htmlFor="email-input" data-testid="email-input">
+            Email:
+            <input
+              /* O email está no formato válido, como 'alguem@alguem.com'. */
+              type="email"
+              value={ email }
+              id="email-input"
+              onChange={ this.handleInput }
+              name="email"
+            />
+          </label>
+          <label htmlFor="password-input" data-testid="password-input">
+            Password:
+            <input
+              type="password"
+              value={ password }
+              id="password-input"
+              onChange={ this.handleInput }
+              name="password"
+            />
+          </label>
+          {/* Crie um botão com o texto ‘Entrar’. */}
+          <button
+            type="button"
+            onClick={ this.handleSubmit }
+            /* A senha possui 6 ou mais caracteres. */
+            disabled={ password.length < passwordLength }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
