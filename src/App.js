e291b7519ad90071ce4ec684abd63351e39f0520
Roberto Natividade <robertonnv@gmail.com>
2021-06-14 17:31:30 -0300

    Requisito

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 // import user from './reducers/user';
 
 function App() {
   return (
-    <div>
-      <h1>
-        Hello, TrybeWallet!!
-      </h1>
-      <Login />
-    </div>);
+    <Switch>
+      <Route exact path="/" component={ Login } />
+    </Switch>
+  );
 }
 
 export default App;
