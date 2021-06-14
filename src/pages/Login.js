2b74cad0a27340f6ffa5c7df3a7f3fb537d5dc00
Robertson <robertson.nunes@gmail.com>
2021-06-14 13:49:04 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // 1. Crie uma página inicial de login com os seguintes campos e características:
 // 2. Realize as seguintes verificações nos campos de email, senha e botão
+
 class Login extends React.Component {
   constructor() {
     super();
@@ -8,13 +10,13 @@ class Login extends React.Component {
     this.handleInput = this.handleInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
+    /* Salve o email no estado da aplicação, com a chave email, assim que a pessoa usuária logar. */
     this.state = {
       email: '',
       password: '',
     };
   }
 
-  /* Salve o email no estado da aplicação, com a chave email, assim que a pessoa usuária logar. */
   handleInput({ target }) {
     const { name, value } = target;
     this.setState({ [name]: value });
@@ -27,16 +29,19 @@ class Login extends React.Component {
   render() {
     const { email, password } = this.state;
     const passLength = 6;
+    /* https://www.w3resource.com/javascript/form/email-validation.php */
+    /* O email está no formato válido, como 'alguem@alguem.com'. */
+    const vEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
     return (
       <div>
         <h1>Login</h1>
         <form>
           {/* Você deve criar um local para que a pessoa usuária insira seu email e senha.
           */}
-          <label htmlFor="email-input" data-testid="email-input">
+          <label htmlFor="email-input">
             Email:
             <input
-              /* O email está no formato válido, como 'alguem@alguem.com'. */
+              data-testid="email-input"
               type="email"
               value={ email }
               id="email-input"
@@ -44,9 +49,10 @@ class Login extends React.Component {
               name="email"
             />
           </label>
-          <label htmlFor="password-input" data-testid="password-input">
+          <label htmlFor="password-input">
             Password:
             <input
+              data-testid="password-input"
               type="password"
               value={ password }
               id="password-input"
@@ -54,15 +60,18 @@ class Login extends React.Component {
               name="password"
             />
           </label>
-          {/* Crie um botão com o texto ‘Entrar’. */}
-          <button
-            type="button"
-            onClick={ this.handleSubmit }
-            /* A senha possui 6 ou mais caracteres. */
-            disabled={ password.length < passLength }
-          >
-            Entrar
-          </button>
+          {/* A rota deve ser mudada para '/carteira' após o clique no botão 'Entrar'. */}
+          <Link to="/carteira">
+            {/* Crie um botão com o texto ‘Entrar’. */}
+            <button
+              type="button"
+              onClick={ this.handleSubmit }
+              /* A senha possui 6 ou mais caracteres. */
+              disabled={ (vEmail.test(email) === false) || password.length < passLength }
+            >
+              Entrar
+            </button>
+          </Link>
         </form>
       </div>
     );
