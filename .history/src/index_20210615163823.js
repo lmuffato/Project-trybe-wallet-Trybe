e57383d2ae0849313acef4c900dd97f1b85f41d1
Juan Pablo <juan.tentiomaru9309@gmail.com>
2021-06-15 18:02:55 -0300

    req2 só que agora salvo

--- .history/src/index_20210615163823.js
+++ .history/src/index_20210615163823.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
