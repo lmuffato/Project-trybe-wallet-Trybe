4695062302af1e8799be1dc5fb54711125535138
w-cypriano <profwellington17@gmail.com>
2021-06-15 01:30:55 -0300

    fix Requisito 6 e 7

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -20,12 +20,13 @@ class Form extends React.Component {
   }
 
   selectCurrency(currencies) {
+    const { Moeda } = this.state;
     delete currencies.USDT;
     const currenciesCode = Object.values(currencies);
     return (
       <label htmlFor="Moeda">
         Moeda:
-        <select name="Moeda" onChange={ this.handleChange }>
+        <select value={ Moeda } name="Moeda" onChange={ this.handleChange }>
           {currenciesCode.map((currency, index) => (
             <option key={ index }>
               {currency.code}
@@ -38,21 +39,32 @@ class Form extends React.Component {
 
   render() {
     const { currencies } = this.props;
+    const { Valor, Descrição, Pagamento, Tag } = this.state;
     return (
       <div>
         <form>
           <label htmlFor="valor">
-            valor:
-            <input type="text" name="Valor" onChange={ this.handleChange } />
+            Valor:
+            <input
+              type="text"
+              value={ Valor }
+              name="Valor"
+              onChange={ this.handleChange }
+            />
           </label>
           <label htmlFor="Descrição">
             Descrição:
-            <input type="text" name="Descrição" onChange={ this.handleChange } />
+            <input
+              type="text"
+              value={ Descrição }
+              name="Descrição"
+              onChange={ this.handleChange }
+            />
           </label>
           { this.selectCurrency(currencies) }
           <label htmlFor="Modo de pagamento">
             Método de pagamento:
-            <select name="Pagamento" onChange={ this.handleChange }>
+            <select value={ Pagamento } name="Pagamento" onChange={ this.handleChange }>
               <option value="Dinheiro">Dinheiro</option>
               <option value="Crédito">Cartão de crédito</option>
               <option value="Debito">Cartão de débito</option>
@@ -60,7 +72,7 @@ class Form extends React.Component {
           </label>
           <label htmlFor="Tag">
             Tag:
-            <select name="Tag" onChange={ this.handleChange }>
+            <select value={ Tag } name="Tag" onChange={ this.handleChange }>
               <option value="Alimentação">Alimentação</option>
               <option value="Lazer">Lazer</option>
               <option value="Trabalho">Trabalho</option>
