76a1a874bbbe188ff8f2c3a806d32dddb1bf2b67
Juan Pablo <juan.tentiomaru9309@gmail.com>
2021-06-15 18:42:10 -0300

    req 3 e 2 agora salvos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+function saveUser(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'SAVE_EMAIL':
+    return {
+      ...state.user,
+      email: action.payload.email,
+    };
+  default:
+    return state;
+  }
+}
+export default saveUser;
