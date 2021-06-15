93fb79fe9cc927ab89334e8e4e34b914b870b076
guidugaich <guidugaich.dev@gmail.com>
2021-06-15 13:41:12 -0300

    adding components WalletTableHeader and WalletTableBody to WalletTable

--- src/components/WalletTable.js
+++ src/components/WalletTable.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import WalletTableHeader from './WalletTable/WalletTableHeader';
+import WalletTableBody from './WalletTable/WalletTableBody';
 
 class WalletTable extends Component {
   render() {
     return (
-      <div>
-        Wallet Table
-      </div>
+      <table>
+        <WalletTableHeader />
+        <WalletTableBody />
+      </table>
     );
   }
 }
