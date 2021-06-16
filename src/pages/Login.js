21769d01915069686221788e8fcf6797b92de332
biazidioti <bzferreira.bio@gmail.com>
2021-06-16 11:47:13 -0300

    fix lint feat: req3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import setEmail from '../actions';
@@ -79,3 +80,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 // export default Login;
 export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  handleClick: PropTypes.func,
+}.isRequired;
