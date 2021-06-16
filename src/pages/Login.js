87a0a7345396be4af6bb12d17b388dea895f7af9
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-06-16 00:22:39 -0300

    requisito 2 - Completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+      buttonOk: false,
+    };
+  }
+
+  validateFields() {
+    const CARACTER_MIN = 6;
+    const { password, email } = this.state;
+    const regex = /^[\w.]+@[a-z]+\.\w{2,3}$/g;
+    const buttonOk = password.length >= CARACTER_MIN && regex.test(email);
+    this.setState({ buttonOk });
+  }
+
+  handleChange({ target }) {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
+    }, () => this.validateFields());
+  }
+
   render() {
+    const { email, password, buttonOk } = this.state;
+
     return (
       <div>
         <label htmlFor="email">
@@ -10,8 +38,10 @@ class Login extends React.Component {
             type="email"
             name="email"
             id="email"
+            value={ email }
             placeholder="Digite seu e-mail"
             data-testid="email-input"
+            onChange={ this.handleChange }
           />
         </label>
         <label htmlFor="password">
@@ -20,11 +50,18 @@ class Login extends React.Component {
             type="password"
             name="password"
             id="password"
+            value={ password }
             placeholder="Digite sua senha"
             data-testid="password-input"
+            onChange={ this.handleChange }
           />
         </label>
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          disabled={ !buttonOk }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
