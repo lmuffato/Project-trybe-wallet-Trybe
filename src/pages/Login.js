f7690c77eb545296d09b924519e638abd35f90d2
biazidioti <bzferreira.bio@gmail.com>
2021-06-16 09:28:43 -0300

    Req 2.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,64 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      senha: '',
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  validation() {
+    const { email, senha } = this.state;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const minValue = 6;
+    if (emailRegex.test(email) && senha.length >= minValue) {
+      return false;
+    }
+
+    return true;
+  }
+
   render() {
+    const { email, senha } = this.state;
     return (
       <>
         <div>Login</div>
         <label htmlFor="input-email">
-          <input data-testid="email-input" type="text" id="input-email" />
+          <input
+            data-testid="email-input"
+            type="text"
+            id="input-email"
+            onChange={ this.onChange }
+            value={ email }
+            name="email"
+          />
         </label>
         <label htmlFor="input-password">
-          <input data-testid="password-input" type="password" id="input-password" />
+          <input
+            data-testid="password-input"
+            type="password"
+            id="input-password"
+            onChange={ this.onChange }
+            value={ senha }
+            name="senha"
+          />
         </label>
-        <button type="button">Entrar</button>
+        <button
+          disabled={ this.validation() }
+          type="button"
+        >
+          Entrar
+        </button>
       </>
     );
   }
