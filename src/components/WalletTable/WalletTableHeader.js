8a71707b45eec3a7331a70a585940aca47c68f34
guidugaich <guidugaich.dev@gmail.com>
2021-06-16 13:30:09 -0300

    thead element to avoid warnings

--- src/components/WalletTable/WalletTableHeader.js
+++ src/components/WalletTable/WalletTableHeader.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 class WalletTableHeader extends Component {
   render() {
     return (
-      <tr>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
     );
   }
 }
