c8eb7b8b653192ad1b69d512d5409a06e4485bec
Leonnardo Lair <leonnardo0316@gmail.com>
2021-06-17 04:33:25 -0300

    lint fixes

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>  
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,79 +20,80 @@ class Login extends React.Component {
   }
 
   emailPattern() {
-    return "[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*"
+    return /\S+@\S+\.\S+/;
   }
 
-
-/* ideia para verificação de email tirada daqui:
-https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript*/
+  /* ideia para verificação de email tirada daqui:
+https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript?page=1&tab=votes#tab-top */
 
   handleChanges(a) {
-    const { passCheck, emailCheck } = this.state
-    if (a.type === "email") {
-      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const emailchecker = re.test(String(a.value).toLowerCase());
-    if (emailchecker) {
-      this.setState({
-        
-            email: a.value,
-            emailCheck: true
-          })
-    }  
-    }
-    if (a.type === "password") {
-      const test = a.value;
-      if (test.length >= 5 ) {
+    const { passCheck, emailCheck } = this.state;
+    if (a.type === 'email') {
+      const re = /\S+@\S+\.\S+/;
+      const emailchecker = re.test(String(a.value).toLowerCase());
+      if (emailchecker) {
         this.setState({
-            passCheck: true
-          })
+          email: a.value,
+          emailCheck: true,
+        });
       }
     }
-    if ( passCheck && emailCheck) {
+    if (a.type === 'password') {
+      const test = a.value;
+      const minPassword = 5;
+      if (test.length >= minPassword) {
+        this.setState({
+          passCheck: true,
+        });
+      }
+    }
+    if (passCheck && emailCheck) {
       this.setState({
-        buttonDisable: false
-      })
+        buttonDisable: false,
+      });
     }
   }
 
-  handleLogin(a) {
+  handleLogin() {
     const { userLogin } = this.props;
-    const { email } = this.state
+    const { email } = this.state;
     const loginToken = {
       email,
-    }
+
+    };
     this.setState({
       shoudRedirec: true,
-    })
+    });
     userLogin(loginToken);
   }
 
   render() {
-    const { shoudRedirec , buttonDisable} = this.state
-    if (shoudRedirec) return <Redirect to="/carteira"/>
-    return <>
-    <form>
-    <input
-    type='email'
-    onChange={(event) => this.handleChanges(event.target)}
-    data-testid="email-input"
-    placeholder="Enter your email"
-    pattern={this.emailPattern()}>
-    </input>
-    <input
-    type="password"
-    onChange={(event) => this.handleChanges(event.target)}
-    data-testid="password-input"
-    placeholder="Enter your Password"
-    minLength="6">
-    </input>
-    <button
-    onClick={() => this.handleLogin()}
-    disabled={buttonDisable}>
-      Entrar
-    </button>
-    </form>
-    </>;
+    const { shoudRedirec, buttonDisable } = this.state;
+    if (shoudRedirec) return <Redirect to="/carteira" />;
+    return (
+      <form>
+        <input
+          type="email"
+          onChange={ (event) => this.handleChanges(event.target) }
+          data-testid="email-input"
+          placeholder="Enter your email"
+          pattern={ this.emailPattern() }
+        />
+        <input
+          type="password"
+          onChange={ (event) => this.handleChanges(event.target) }
+          data-testid="password-input"
+          placeholder="Enter your Password"
+          minLength="6"
+        />
+        <button
+          type="button"
+          onClick={ () => this.handleLogin() }
+          disabled={ buttonDisable }
+        >
+          Entrar
+        </button>
+      </form>);
   }
 }
 
