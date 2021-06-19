f1df0b21b3bd212e6b38cdee2492b8c18b6b9fa9
Renata Andrade <reapminas@gmail.com>
2021-06-19 19:02:04 -0300

    feat: requisito 5 - cria header carteira - parcial

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <section>
+        <header>
+          <div>TrybeWallet</div>
+          <h3 data-testid="email-field">Email</h3>
+          <h3 data-testid="total-field">Total Despesas</h3>
+          <h3 data-testid="header-currency-field">BRL</h3>
+        </header>
+      </section>
+    );
   }
 }
 
