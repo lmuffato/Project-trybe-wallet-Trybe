19b535f1368bc9b145dc512cbd418acf958cdf60
biazidioti <bzferreira.bio@gmail.com>
2021-06-19 12:41:56 -0300

    Change function to class.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/carteira" component={ Wallet } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/carteira" component={ Wallet } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
