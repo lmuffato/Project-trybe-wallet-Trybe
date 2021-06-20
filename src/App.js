c94853e5bb962ce11483f463d7003ddb5eb8d12d
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-20 15:06:17 -0300

    Feat: Form input for login and password and button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import './App.css';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/wallet">
+          <Wallet />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+
+      <section className="loginContainer">
+        <header className="loginHeader">
+          <h1>Trybe Wallet</h1>
+        </header>
+        <form className="login">
+          <input type="text" placeholder="Email" testid="email-input" />
+          <input type="password" placeholder="Password" testid="password-input" />
+          <input className="button" type="button" value="Entrar" />
+        </form>
+      </section>
+    );
   }
 }
 
