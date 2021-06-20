3b9bd9aaad0e83b107a8fb15c47c2470db695935
ThalitaC <thalitaceci@gmail.com>
2021-06-20 13:49:07 -0300

    feat: req1-login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import InputLogin from '../components/InputLogin';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section className="container">
+        <main className="card">
+          <h1 className="card-header">TrybeWallet</h1>
+          <InputLogin />
+          <button
+            className="enter-bttn"
+            type="submit"
+          >
+            <h3>Entrar</h3>
+          </button>
+        </main>
+      </section>
+    );
   }
 }
 
