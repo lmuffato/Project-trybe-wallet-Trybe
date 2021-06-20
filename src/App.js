0267c5019806da2566575f644bfb58ed4c4a3681
getuliogutemberg <getulio.dev@gmail.com>
2021-06-20 15:44:53 -0300

    resolve requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <h1>
+        Hello, TrybeWallet!
+      </h1>
+
+      <Route path="/" exact component={ Login } />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h2>Login</h2>
+
+        <input type="email" data-testid="email-input" />
+
+        <input type="password" data-testid="password-input" />
+
+        <Link to="/">
+          <button type="submit">Entrar</button>
+        </Link>
+
+      </div>
+    );
   }
 }
 
