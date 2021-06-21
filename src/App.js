544f02e49825e782d7b5b8f303cc1a195ea19ffe
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-06-21 15:17:14 -0300

    feat: rotas no App, compon e pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
-import NotFound from './components/NotFound';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={ Home } />
-      <Route path="/login" component={ Login } />
+      <Route path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
       <Route component={ NotFound } />
     </Switch>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        Pagina não encontrada!
+      </div>
+    );
+  }
+}
+
+export default NotFound;
