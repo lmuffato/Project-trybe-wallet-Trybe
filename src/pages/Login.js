4bf948e22b0d4691141accf5113d56f4b0158235
Renata Andrade <reapminas@gmail.com>
2021-06-20 22:41:45 -0300

    fix: requisito 5 - cria header carteira - final

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -101,3 +101,5 @@ export default connect(null, mapDispatchToProps)(Login);
 // Como atualizar a página quando clicar no botão: https://stackoverflow.com/questions/42701129/how-to-push-to-history-in-react-router-v4
 // Consultas e aproveitamento de código e lógica do projeto Trivia do meu grupo (turma 10 - grupo 8)
 // Como validar um email: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+// Consultas aos repositórios: https://github.com/tryber/sd-010-a-project-trybewallet/pull/67/commits/f151c2b7111f4e8f0092308c9bb5ea77bdcc2a14
+//                             https://github.com/tryber/sd-010-a-project-trybewallet/pull/73/commits/f3cfe9785e6717db8205a94d09fc174d3e021c00
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { string } from 'prop-types';
 import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
+    console.log(email);
     return (
       <section>
         <header>
           <div>TrybeWallet</div>
-          <h3 data-testid="email-field">Email</h3>
-          <h3 data-testid="total-field">Total Despesas</h3>
+          <h3 data-testid="email-field">{ email }</h3>
+          <h3 data-testid="total-field">0</h3>
           <h3 data-testid="header-currency-field">BRL</h3>
         </header>
         <Form />
@@ -17,4 +21,11 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email });
+
+export default connect(mapStateToProps, null)(Wallet);
