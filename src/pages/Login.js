be5477a500f4cfe7c6ad88e35f6a28867c9722e3
jlfagundes <jlfagundes@gmail.com>
2021-06-22 09:56:04 -0300

    feat: (req2) test password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,15 +9,18 @@ class Login extends React.Component {
     this.state = {
       email: '',
       buttonDisable: true,
+      password: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.validateLogin = this.validateLogin.bind(this);
+    // this.compareLogin = this.compareLogin.bind(this);
   }
 
-  handleChange(event) {
+  handleChange({ target }) {
+    const { name, value } = target;
     this.setState({
-      email: event.target.value,
+      [name]: value,
     });
     this.validateLogin();
   }
@@ -25,21 +28,34 @@ class Login extends React.Component {
   // trecho do código realizado com solução encontrada por Gabriel Pereira no link https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
   testEmail(email) {
     const re = /\S+@\S+\.\S+/;
-    const retorno = re.test(email);
-    return retorno;
+    return re.test(email);
   }
 
   validateLogin() {
-    const { email } = this.state;
-    if (this.testEmail(email)) return this.setState({ buttonDisable: false });
+    const { email, password } = this.state;
+    if (this.testEmail(email) && this.testPassword(password)) {
+      return this.setState({ buttonDisable: false });
+    }
     return this.setState({ buttonDisable: true });
   }
 
+  testPassword() {
+    const { password } = this.state;
+    const passwordLength = 5;
+    console.log(password);
+    if (password > passwordLength) return true;
+    return false;
+  }
+
+  // compareLogin() {
+  //   if (this.testPassword() && this.validateLogin()) {
+  //     return this.setState({ buttonDisable: false });
+  //   }
+  //   return this.setState({ buttonDisable: true });
+  // }
+
   render() {
-    const { email, buttonDisable } = this.state;
-    console.log(email);
-    // console.log(this.testEmail(email));
-    // console.log(this.validateLogin());
+    const { buttonDisable } = this.state;
     return (
       <div>
         login
@@ -58,6 +74,7 @@ class Login extends React.Component {
               data-testid="password-input"
               placeholder="password"
               name="password"
+              onChange={ this.handleChange }
             />
           </label>
           <button
