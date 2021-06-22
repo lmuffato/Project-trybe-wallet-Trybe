111829a8145ff053e670bbc8acd0ac73ad713198
biazidioti <bzferreira.bio@gmail.com>
2021-06-22 08:48:59 -0300

    feat: req 5 - set email as state.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
-        <span data-testid="email-field"> Email: </span>
+        <span data-testid="email-field">
+          { email }
+        </span>
         <span data-testid="total-field"> Total = 0</span>
-        <span data-testid="header-currency-field"> $ </span>
+        <span data-testid="header-currency-field"> BRL </span>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
