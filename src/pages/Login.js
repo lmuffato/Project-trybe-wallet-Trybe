2f91c3dde4f54616c44411b80b46fc41bd40da39
getuliogutemberg <getulio.dev@gmail.com>
2021-06-22 10:54:23 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,17 +2,46 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <h2>Login</h2>
 
-        <input type="email" data-testid="email-input" />
+        <input
+          type="email"
+          data-testid="email-input"
+          onChange={
+            (event) => this.setState({ email: event.target.value })
+          }
+        />
 
-        <input type="password" data-testid="password-input" />
+        <input
+          type="password"
+          data-testid="password-input"
+          onChange={
+            (event) => this.setState({ password: event.target.value })
+          }
+        />
 
         <Link to="/">
-          <button type="submit">Entrar</button>
+          <button
+            type="submit"
+            onClick={
+              () => console.log(email, password)
+            }
+          >
+            Entrar
+          </button>
         </Link>
 
       </div>
