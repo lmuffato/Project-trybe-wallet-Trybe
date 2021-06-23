1cceb087dc14322cb667e4bbd75f1f46604d9e79
getuliogutemberg <getulio.dev@gmail.com>
2021-06-23 18:25:02 -0300

    importa Proptypes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import userLogin from '../actions';
 
 class Login extends React.Component {
