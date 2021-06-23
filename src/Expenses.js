f6507da5b2340160a224c96040a897b1494590df
Roberto Natividade <robertonnv@gmail.com>
2021-06-23 12:53:14 -0300

    requisito

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import fetchAPI from './services/index';
+import fetchAPI from './services/fetchAPI';
 import { addExpense, requestApi } from './actions';
 
 class Expenses extends Component {
