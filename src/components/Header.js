ec26685c863d07d8b538b0839a4fa551a9b91c1a
diegoCorreia <diegomethal@gmail.com>
2021-06-23 18:11:42 -0300

    requisito 6

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,49 @@ class Header extends React.Component {
     const { email } = this.props;
     return (
       <div>
-        <span data-testid="email-field">{`Email: ${email}`}</span>
-        <span data-testid="total-field">Despesa total: R$ 0</span>
-        <span data-testid="header-currency-field">BRL</span>
+        <div>
+          <span data-testid="email-field">{`Email: ${email}`}</span>
+          <span data-testid="total-field">Despesa total: R$ 0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </div>
+        <form>
+          <label htmlFor="valor">
+            Valor:
+            <input type="number" id="valor" />
+          </label>
+
+          <label htmlFor="descricao">
+            Descrição:
+            <input type="text" id="descricao" />
+          </label>
+
+          <label htmlFor="moeda">
+            Moeda:
+            <select id="moeda">
+              <option value="0">0</option>
+            </select>
+          </label>
+
+          <label htmlFor="pagamento">
+            Método de pagamento
+            <select id="pagamento">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
