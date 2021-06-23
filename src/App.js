a8d5fcd711dfe07a03d43346a3fec82c84814e7c
pauloeliezerg <pauloeliezerg@gmail.com>
2021-06-22 21:56:15 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Provider>
+      <div>Hello, TrybeWallet!</div>
+    </Provider>
+  );
 }
 
 export default App;
