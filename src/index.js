7c902eee407399b59b95a1e72aa7374a86ff3406
Roberto Natividade <robertonnv@gmail.com>
2021-06-23 12:34:29 -0300

    requisito

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={ store }>
