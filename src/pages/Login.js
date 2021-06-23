1a39c596648cb79c41de604b1af56ab263d2be47
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-06-23 08:31:41 -0300

    Adicionando Store

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="name">
+            E-mail:
+            <input
+              type="email"
+              name="name"
+              data-testid="email-input"
+            />
+
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+            />
+
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
