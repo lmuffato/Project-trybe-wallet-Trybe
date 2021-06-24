3771bd63c2e7bdb33bde08ab1a0c3e82e9abbd91
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-06-24 11:51:09 -0300

    add req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,74 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.hundleChange = this.hundleChange.bind(this);
+  }
+
+  hundleChange({ target }) {
+    if (target.type === 'email') {
+      this.setState({ email: target.value });
+    } else if (target.type === 'password') {
+      this.setState({ password: target.value });
+    }
+  }
+
+  renderButtonDesabled() {
+    return (
+      <button
+        type="button"
+        disabled
+      >
+        Entrar
+      </button>
+    );
+  }
+
+  renderButtonEnabled() {
+    return (
+      <Link to="/carteira">
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </Link>
+    );
+  }
+
   render() {
+    const { email, password } = this.state;
+    const emailModel = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const passwordMinLength = 6;
+
     return (
       <div>
         <form>
           <input
             type="email"
             data-testid="email-input"
+            value={ email }
+            onChange={ this.hundleChange }
           />
           <input
             type="password"
             data-testid="password-input"
+            value={ password }
+            onChange={ this.hundleChange }
           />
         </form>
-        <button type="button">Entrar</button>
+        {
+          password.length >= passwordMinLength && email.match(emailModel)
+            ? this.renderButtonEnabled()
+            : this.renderButtonDesabled()
+        }
       </div>
     );
   }
