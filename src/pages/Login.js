8d8f3106d77f909f2874345f8402871f15f9d745
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-06-24 11:12:52 -0300

    add req 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+          />
+        </form>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
