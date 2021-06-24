e148d2ebf0a3bc58867290c2453996a386bf7936
pauloeliezerg <pauloeliezerg@gmail.com>
2021-06-23 23:47:06 -0300

    Req06

---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="value">
+          Valor:
+          <input type="text" name="value" />
+        </label>
+        <label htmlFor="description">
+          Descrição:
+          <input type="text" name="description" />
+        </label>
+        <label htmlFor="currency">
+          Moeda:
+          <select name="currency">
+            { }
+          </select>
+        </label>
+        <label htmlFor="payment">
+          Método de pagamento:
+          <select name="payment">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag:
+          <select name="tag">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
+      </form>
+
+    );
+  }
+}
+
+export default Form;
