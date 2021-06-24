73774b17b06493a1964b84e206fe122abf88465e
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-06-24 09:36:43 -0300

    feat: req 5 - 8

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,43 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { currencies } from '../actions';
 
 class Table extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0,
-    };
-
-    this.updateTable = this.updateTable.bind(this);
-    this.qualquer = this.qualquer.bind(this);
-  }
-
-  qualquer() {
-    const { wallet } = this.props;
-    console.log(Object.entries(wallet));
-    if (Object.entries(wallet) === []) {
-      return undefined;
-    }
-    return (
-      this.updateTable()
-    );
-  }
-
-  updateTable() {
-    const { wallet } = this.props;
-    return (
-      <table>
-        <td>{wallet.value}</td>
-        <td>{wallet.description}</td>
-        <td>{wallet.currency}</td>
-        <td>{wallet.tag}</td>
-        <td>editar</td>
-      </table>
-    );
-  }
-
   render() {
     return (
       <table>
@@ -45,7 +10,6 @@ class Table extends Component {
         <th>Descrição</th>
         <th>Moeda</th>
         <th>Categoria</th>
-        { this.updateTable() }
       </table>
     );
   }
@@ -59,12 +23,5 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   toCurrency: (payload) => dispatch(currencies(payload)),
 });
-
-Table.propTypes = {
-  toCurrency: PropTypes.oneOf(
-    PropTypes.number,
-    PropTypes.array,
-  ).isRequired,
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Table);
