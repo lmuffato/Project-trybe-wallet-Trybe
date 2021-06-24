fef2b1f3b30fea8be6424b37b54acfc10a558b19
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-06-23 23:45:54 -0300

    Req 6, Lint

--- src/services/api/servicesApi.js
+++ src/services/api/servicesApi.js
@@ -1,5 +1,5 @@
 export default fetchByCurrency = () => { // nova requisição contendo o valor id do item selecionado.
-  fetch("https://economia.awesomeapi.com.br/json/all")
+  fetch('https://economia.awesomeapi.com.br/json/all')
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
