d010a40324ab4fb96f0073df255790f02372ab4f
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-06-24 15:34:54 -0300

    add req 4 and req 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,11 +6,47 @@ class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <h2 data-testid="email-field">{ email }</h2>
-        <span data-testid="total-field">0</span>
-        <span data-testid="header-currency-field">BRL</span>
-      </header>
+      <div>
+        <header>
+          <h2 data-testid="email-field">{ email }</h2>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input type="number" id="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" id="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select id="currency">
+              <option value="1">0</option>
+            </select>
+          </label>
+          <label htmlFor="payment-method">
+            Método de pagamento
+            <select id="payment-method">
+              <option value="cash">Dinheiro</option>
+              <option value="credit-card">Cartão de crédito</option>
+              <option value="debit-card">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option value="food">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transport">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
