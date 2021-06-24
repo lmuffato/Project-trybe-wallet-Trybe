d94d3ec645f0f88af24f1488e7239859c4424efa
Apoc35 <alexv_silveira@outlook.com>
2021-06-24 20:12:40 -0300

    Requisito 2.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       Hello, TrybeWallet!
       <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
     </div>
   );
 }
