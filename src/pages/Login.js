03708f11ba50f4f8c9b978a96ae6d7eed37187ed
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-24 18:29:16 -0300

    Feat: Adding email and password validation.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,83 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
-  render() {
-    return (
+  constructor() {
+    super();
+    this.passwordIsValid = this.passwordIsValid.bind(this);
+    this.emailIsValid = this.emailIsValid.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 
+    this.state = {
+      email: '',
+      password: '',
+      enabledButton: true,
+    };
+  }
+
+  emailIsValid() {
+    const { email } = this.state;
+
+    return email.match(/\S+@\S+\.\S+/);
+  }
+
+  passwordIsValid() {
+    const { password } = this.state;
+    const SIX = 6;
+    return password.length >= SIX;
+  }
+
+  handleChange({ target: { type, value } }) {
+    this.setState({
+      [type]: value }, () => {
+      if (this.emailIsValid() && this.passwordIsValid()) {
+        this.setState({ enabledButton: false });
+      } else {
+        this.setState({ enabledButton: true });
+      }
+    });
+  }
+
+  render() {
+    const { email, password, enabledButton } = this.state;
+
+    return (
       <section className="loginContainer">
         <header className="loginHeader">
           <h1>Trybe Wallet</h1>
         </header>
-        <form className="login">
-          <input type="text" placeholder="Email" data-testid="email-input" />
-          <input type="password" placeholder="Password" data-testid="password-input" />
-          <input className="button" type="button" value="Entrar" />
-        </form>
+        <section className="login">
+          <input
+            type="email"
+            placeholder="Email"
+            value={ email }
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <Link to="/carteira">
+            <input
+              className="button"
+              type="button"
+              value="Entrar"
+              disabled={ enabledButton }
+            />
+          </Link>
+        </section>
       </section>
     );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,19 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 import { SAVE_EMAIL } from '../actions/index';
 
-const user = (state = '', action) => {
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case SAVE_EMAIL:
-    return { email: action.payload.value };
+    return {
+      ...state,
+      [action.payload.user.email]: action.payload.value,
+    };
   default:
     return state;
   }
