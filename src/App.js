5cd87aff81720dbbc14fb76bf162971292001633
pauloeliezerg <pauloeliezerg@gmail.com>
2021-06-24 04:21:15 -0300

    Req03 fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router';
-import store from './store';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
-function App() {
-  return (
-    <Provider store={ store }>
+class App extends React.Component {
+  render() {
+    return (
       <Switch>
         <Route exact path="/carteira" component={ Wallet } />
         <Route path="/" component={ Login } />
       </Switch>
-    </Provider>
-  );
+    );
+  }
 }
 
 export default App;
