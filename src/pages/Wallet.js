3b88434c6b7aee0171e79b822c20bff47efb250e
Junior Portela <portelajr14@gmail.com>
2021-06-24 01:08:26 -0300

    fix: refactoring on page wallet form

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,6 +3,75 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+    //   valor: '',
+    // };
+
+    this.renderInputValue = this.renderInputValue.bind(this);
+    this.renderInputDescription = this.renderInputDescription.bind(this);
+    this.renderExchangeSelect = this.renderExchangeSelect.bind(this);
+    this.renderPaymentSelect = this.renderPaymentSelect.bind(this);
+    this.renderTag = this.renderTag.bind(this);
+  }
+
+  renderInputValue() {
+    return (
+      <label htmlFor="valor">
+        Valor
+        <input type="text" id="valor" value="" />
+      </label>
+    );
+  }
+
+  renderInputDescription() {
+    return (
+      <label htmlFor="descricao">
+        Descrição
+        <input type="text" id="descricao" />
+      </label>
+    );
+  }
+
+  renderExchangeSelect() {
+    return (
+      <label htmlFor="moeda">
+        Moeda
+        <select id="moeda">Opções de moeda</select>
+      </label>
+    );
+  }
+
+  renderPaymentSelect() {
+    return (
+      <label htmlFor="method">
+        Método de Pagamento
+        <select id="method">
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderTag() {
+    return (
+      <label htmlFor="tag">
+        Tag
+        <select id="tag">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+          <option>Trabalho</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const { email } = this.props;
 
@@ -13,38 +82,12 @@ class Wallet extends React.Component {
           <p data-testid="total-field">0</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
-        <h1>PÁGINA CARTEIRA</h1>
         <form>
-          <label htmlFor="valor">
-            Valor
-            <input type="text" id="valor" value="" />
-          </label>
-          <label htmlFor="descricao">
-            Descrição
-            <input type="text" id="descricao" />
-          </label>
-          <label htmlFor="moeda">
-            Moeda
-            <select id="moeda">Opções de moeda</select>
-          </label>
-          <label htmlFor>
-            Método de Pagamento
-            <select>
-              <option>Dinheiro</option>
-              <option>Cartão de crédito</option>
-              <option>Cartão de débito</option>
-            </select>
-          </label>
-          <label htmlFor>
-            Tag
-            <select>
-              <option>Alimentação</option>
-              <option>Lazer</option>
-              <option>Transporte</option>
-              <option>Saúde</option>
-              <option>Trabalho</option>
-            </select>
-          </label>
+          { this.renderInputValue() }
+          { this.renderInputDescription() }
+          { this.renderExchangeSelect() }
+          { this.renderPaymentSelect() }
+          { this.renderTag() }
         </form>
       </div>
     );
---
+++ src/services/localData.js
@@ -0,0 +1,18 @@
+export const paymentMethods = ['Dinheiro', 'Cartão de Crédito', 'Cartão Débito'];
+export const tags = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+export const exchangeOptions = [
+  'USD',
+  'CAD',
+  'EUR',
+  'GBP',
+  'ARS',
+  'BTC',
+  'LTC',
+  'JPY',
+  'CHF',
+  'AUD',
+  'CNY',
+  'ILS',
+  'ETH',
+  'XRP',
+];
