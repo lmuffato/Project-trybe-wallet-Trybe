b8d64775090e16914f25adf0017deeed9b9eee4d
johnpligeiro <johnpligeiro@gmail.com>
2021-06-25 06:20:48 -0300

    req8

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchWalletSpend } from '../actions';
 
 class AddExpense extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       arrayObjCoin: [],
+      spent: 0,
+      coin: 'USD',
+      paymentMethod: '',
+      tag: '',
+      description: '',
     };
 
     this.coin = this.coin.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.buttonAddSpend = this.buttonAddSpend.bind(this);
   }
 
   componentDidMount() {
@@ -16,29 +26,31 @@ class AddExpense extends React.Component {
 
   async getCoin() {
     const data = await fetch('https://economia.awesomeapi.com.br/json/all');
-    const arrayCoins = await data.json();
-    delete arrayCoins.USDT;
+    const objCoins = await data.json();
+    delete objCoins.USDT;
 
-    const arrayObjCoin = Object.values(arrayCoins);
+    const arrayObjCoin = Object.values(objCoins);
     this.setState({ arrayObjCoin });
   }
 
-  spend() {
+  handleClick({ target: { id, value } }) {
+    this.setState({ [id]: value });
+  }
+
+  spend(handleClick, valueState) {
     return (
       <label htmlFor="spent">
         valor:
-        <input type="text" id="spent" />
+        <input type="number" id="spent" value={ valueState } onChange={ handleClick } />
       </label>
     );
   }
 
-  coin() {
-    const { arrayObjCoin } = this.state;
-
+  coin(handleClick, valueState, arrayObjCoin) {
     return (
       <label htmlFor="coin">
         moeda:
-        <select type="text" id="coin">
+        <select type="text" id="coin" onChange={ handleClick } value={ valueState }>
           {arrayObjCoin.map(({ code }, i) => (
             <option key={ i } value={ code }>{code}</option>
           ))}
@@ -47,56 +59,98 @@ class AddExpense extends React.Component {
     );
   }
 
-  paymentMethod() {
+  paymentMethod(handleClick, valueState) {
     return (
       <label htmlFor="paymentMethod">
         método de pagamento:
-        <select type="text" id="paymentMethod">
-          <option value="money">Dinheiro</option>
-          <option value="credit">Crédito</option>
-          <option value="debit">Débito</option>
+        <select
+          type="text"
+          id="paymentMethod"
+          onChange={ handleClick }
+          value={ valueState }
+        >
+          <option value="Dinheiro">Dinheiro</option>
+          <option value="Cartão de crédito">Crédito</option>
+          <option value="Cartão de débito">Débito</option>
         </select>
       </label>
     );
   }
 
-  tag() {
+  tag(handleClick, valueState) {
     return (
       <label htmlFor="tag">
         tag:
-        <select type="text" id="tag">
-          <option value="money">Alimentação</option>
-          <option value="credit">Lazer</option>
-          <option value="debit">Trabalho</option>
-          <option value="debit">Transporte</option>
-          <option value="debit">Saúde</option>
+        <select type="text" id="tag" onChange={ handleClick } value={ valueState }>
+          <option value="Alimentação">Alimentação</option>
+          <option value="Lazer">Lazer</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Transporte">Transporte</option>
+          <option value="Saúde">Saúde</option>
         </select>
       </label>
     );
   }
 
-  description() {
+  description(handleClick, valueState) {
     return (
       <label htmlFor="description">
         descrição:
-        <input type="" id="description" />
+        <input type="" id="description" onChange={ handleClick } value={ valueState } />
       </label>
+    );
+  }
+
+  buttonAddSpend() {
+    const { coin, spent, paymentMethod, tag, description } = this.state;
+    const { addSpend, qtdSpended } = this.props;
+
+    return (
+      <button
+        type="button"
+        onClick={ () => addSpend({
+          id: qtdSpended,
+          value: spent,
+          method: paymentMethod,
+          currency: coin,
+          description,
+          tag,
+        }) }
+      >
+        Adicionar despesa
+      </button>
     );
   }
 
   render() {
+    const { arrayObjCoin, coin, spent, paymentMethod, tag, description } = this.state;
     return (
-      <div className="add-dispense">
+      <div className="add-spend">
         <form>
-          {this.spend()}
-          {this.coin()}
-          {this.paymentMethod()}
-          {this.tag()}
-          {this.description()}
+          {this.spend(this.handleClick, spent)}
+          {this.coin(this.handleClick, coin, arrayObjCoin)}
+          {this.paymentMethod(this.handleClick, paymentMethod)}
+          {this.tag(this.handleClick, tag)}
+          {this.description(this.handleClick, description)}
+
+          {this.buttonAddSpend()}
         </form>
       </div>
     );
   }
 }
 
-export default AddExpense;
+const mapDispatchToProps = (dispatch) => ({
+  addSpend: (spend) => dispatch(fetchWalletSpend(spend)),
+});
+
+const mapStateToProps = (state) => ({
+  qtdSpended: state.wallet.expenses.length,
+});
+
+AddExpense.propTypes = {
+  addSpend: PropTypes.func.isRequired,
+  qtdSpended: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddExpense);
