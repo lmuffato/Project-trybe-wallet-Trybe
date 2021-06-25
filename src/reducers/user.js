1670fceaa006225f51a619a20349926c8b30f701
johnpligeiro <johnpligeiro@gmail.com>
2021-06-25 05:48:10 -0300

    req 3 pt2

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,13 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 
+import { EMAIL_USER } from '../actions';
+
 const userDefaul = {
   email: '',
 };
-
 function userReducer(state = userDefaul, action) {
   switch (action.type) {
-  case '':
-    return {};
+  case EMAIL_USER:
+    return {
+      ...state,
+      email: action.email,
+    };
   default:
     return state;
   }
