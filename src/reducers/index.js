9fb724a219928cf465117b88fce6af0499d26334
johnpligeiro <johnpligeiro@gmail.com>
2021-06-25 05:16:57 -0300

    reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,15 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+import { combineReducers } from 'redux';
+
+import userReducer from './user';
+import walletReducer from './wallet';
+
+const reducer = combineReducers({
+  user: userReducer,
+  wallet: walletReducer,
+});
+
+export default reducer;
