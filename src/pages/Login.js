2d0c808ae234c20a3f14a2fbe995110d79ec4ca9
Southier <patrick.southier@hotmail.com>
2021-06-25 01:03:44 -0300

    fix: return redux folders back to src folder

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 import PropTypes from 'prop-types';
-import { userLogin } from '../redux/actions';
+import { userLogin } from '../actions';
 
 class Login extends React.Component {
   constructor(props) {
