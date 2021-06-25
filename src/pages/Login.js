64d75e42b96df81793218d999dca8dcc295b578a
johnpligeiro <johnpligeiro@gmail.com>
2021-06-25 05:15:29 -0300

    Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="form-login">
+        <form>
+          <input
+            type="email"
+            placeholder="E-mail"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            data-testid="password-input"
+          />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
