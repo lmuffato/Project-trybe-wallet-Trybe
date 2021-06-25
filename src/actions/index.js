96a79b4a1d18f05201474be70f7f3d54ddf3ee75
johnpligeiro <johnpligeiro@gmail.com>
2021-06-25 06:17:51 -0300

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,33 @@
 // Coloque aqui suas actions
 
 export const EMAIL_USER = 'EMAIL_USER';
+export const WALLET_SPEND = 'WALLET_SPEND';
 
 export const actionEmail = (email) => ({
   type: EMAIL_USER,
   email,
 });
+
+export const actionWalletSpend = (spend, objCoins) => ({
+  type: WALLET_SPEND,
+  newSpend: {
+    id: spend.id,
+    value: spend.value,
+    description: spend.description,
+    currency: spend.currency,
+    method: spend.method,
+    tag: spend.tag,
+    exchangeRates: objCoins,
+  },
+});
+
+export const fetchWalletSpend = (spend) => async (dispatch) => {
+  const data = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const objCoins = await data.json();
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete-btn
+
+  delete objCoins.USDT;
+
+  dispatch(actionWalletSpend(spend, objCoins));
+};
