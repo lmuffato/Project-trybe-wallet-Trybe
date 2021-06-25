9c9652b95a81c878f2d54e5bef75e4c98ff64785
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-06-25 11:30:13 -0300

    Requisitos do 1 ao 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,104 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import { login as loginAction } from '../actions';
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      shouldRedirect: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validateFields = this.validateFields.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  validateFields(email, password) {
+    const includesAtSign = email.includes('@');
+    const includesDotCom = email.includes('.com');
+    const passwordMaxLength = 6;
+    const isPasswdValid = password.length >= passwordMaxLength;
+    return (
+      includesAtSign
+      && includesDotCom
+      && isPasswdValid
+    );
+  }
+
+  submitForm() {
+    const { email, password } = this.state;
+    const { login } = this.props;
+    if (this.validateFields(email, password)) {
+      login({ email, password });
+      this.setState({
+        email: '',
+        password: '',
+        shouldRedirect: true,
+      });
+    }
+  }
   render() {
-    return <div>Login</div>;
+    const { email, password, shouldRedirect } = this.state;
+
+    if (shouldRedirect) {
+      return <Redirect to="/carteira" />;
+    }
+
+    return (
+      <div className="login">
+        <section className="login-container">
+          <label htmlFor="email">
+            E-mail:
+            <input
+              type="text"
+              data-testid="email-input"
+              name="email"
+              value={ email }
+              onChange={ (e) => this.handleChange(e) }
+            />
+          </label>
+
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              data-testid="password-input"
+              name="password"
+              value={ password }
+              onChange={ (e) => this.handleChange(e) }
+            />
+          </label>
+
+          <button
+            type="button"
+            className="login-button"
+            onClick={ this.submitForm }
+            disabled={ !this.validateFields(email, password) }
+          >
+            Entrar
+          </button>
+        </section>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  login: (e) => dispatch(loginAction(e)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,122 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExpensesThunk as addExpenseAction,
+  deleteExpense as deleteExpenseAction } from '../actions';
 
-class Wallet extends React.Component {
+import Form from '../components/Form';
+import Table from '../components/Table';
+
+class Wallet extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: 0,
+      description: '',
+      tag: '',
+      method: '',
+      currency: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.calcTotalSpent = this.calcTotalSpent.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { wallet: { currentId } } = this.props;
+    const { addExpense } = this.props;
+    addExpense({
+      id: currentId,
+      ...this.state,
+    });
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  handleDelete(expense) {
+    const { deleteExpense } = this.props;
+    deleteExpense(expense);
+  }
+
+  calcTotalSpent() {
+    const { wallet: { expenses } } = this.props;
+    const total = expenses.reduce(
+      (acc, cur) => parseFloat(cur.value * cur.exchangeRates[cur.currency].ask) + acc, 0,
+    );
+    return total;
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail, wallet: { expenses } } = this.props;
+    const {
+      value,
+      description,
+      tag,
+      method,
+      currency,
+    } = this.state;
+    const walletFormProps = {
+      onSubmit: this.handleSubmit,
+      onChange: this.handleChange,
+      value,
+      description,
+      tag,
+      method,
+      currency,
+    };
+
+    return (
+      <div className="wallet">
+        <header>
+          <p data-testid="email-field">
+            {userEmail.email}
+          </p>
+          <p data-testid="total-field">
+            {this.calcTotalSpent()}
+          </p>
+          <legend data-testid="header-currency-field">
+            BRL
+          </legend>
+        </header>
+        <Form
+          { ...walletFormProps }
+        />
+        <Table
+          expenses={ expenses }
+          onDelete={ this.handleDelete }
+        />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  addExpense: PropTypes.func.isRequired,
+  deleteExpense: PropTypes.func.isRequired,
+  wallet: PropTypes.shape({
+    expenses: PropTypes.objectOf(PropTypes.string),
+    totalSpent: PropTypes.number,
+    currentId: PropTypes.number,
+  }).isRequired,
+  userEmail: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user,
+  wallet: state.wallet,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addExpense: (e) => dispatch(addExpenseAction(e)),
+  deleteExpense: (e) => dispatch(deleteExpenseAction(e)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
